0e0b6d6d8758e3f60658945448ea874799abc51d
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2016-11-29 15:00:18 +0100

    ImageHelp Functions

---
+++ src/THNETII.WinApi.Native.ImageHelp/ImageHelpFunctions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Win32.WinApi.Diagnostics.DbgHelp.ImageHelp
+{
+    /// <summary>
+    /// Type that defines the native P/Invoke functions exported by the ImageHelp library.
+    /// </summary>
+    /// <remarks>
+    /// <para>Original MSDN documentation: <a href="https://msdn.microsoft.com/en-us/library/ms680181.aspx">ImageHlp Functions</a></para>
+    /// </remarks>
+    public static class ImageHelpFunctions
+    {
+
+    }
+}

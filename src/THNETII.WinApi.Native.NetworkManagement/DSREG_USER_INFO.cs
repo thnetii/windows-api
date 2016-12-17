49497ba8c3fd97230fc03d3a2009ccd768553e42
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2016-12-17 01:31:54 +0100

    DSREG_USER_INFO structure

---
+++ src/THNETII.WinApi.Native.NetworkManagement/DSREG_USER_INFO.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Microsoft.Win32.WinApi.Networking.NetworkManagement
+{
+    /// <summary>
+    /// Contains information about a user account that is used to join a device to Microsoft Azure Active Directory.
+    /// </summary>
+    /// <remarks>
+    /// <para>Original MSDN documentation page: <a href="https://msdn.microsoft.com/en-us/library/mt481425.aspx">DSREG_USER_INFO structure</a></para>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public class DSREG_USER_INFO
+    {
+        /// <summary>The email address of the user.</summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string pszUserEmail;
+        /// <summary>The identifier of the Microsoft Passport key that is provisioned for the user.</summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string pszUserKeyId;
+        /// <summary>The name of the Microsoft Passport key that is provisioned for the user.</summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string pszUserKeyName;
+    }
+}

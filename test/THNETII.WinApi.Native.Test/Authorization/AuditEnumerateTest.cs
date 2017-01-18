76ff9f8a00ced011218279454c1cf0dde6a8a7f1
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-01-18 18:33:17 +0100

    Authorization Tests: AuditEnumerateCategories function

---
+++ test/THNETII.WinApi.Native.Test/Authorization/AuditEnumerateTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+using static Microsoft.Win32.WinApi.SecurityIdentity.Authorization.AuthorizationFunctions;
+
+namespace THNETII.WinApi.Native.Test.Authorization
+{
+    public class AuditEnumerateTest
+    {
+        [Fact]
+        public void CanEnumerateAuditCategories()
+        {
+            var apiResult = AuditEnumerateCategories(out var auditCategories, out int categoriesReturned);
+            Assert.True(apiResult);
+            using (auditCategories)
+            {
+                var auditCategoriesGuids = auditCategories.MarshalToManagedArray(categoriesReturned);
+            }
+        }
+    }
+}

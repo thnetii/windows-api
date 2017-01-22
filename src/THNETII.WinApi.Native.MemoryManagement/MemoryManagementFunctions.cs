a0ce9adf006c51ddbea1c0e00b075598ad6eb03e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-01-22 17:00:07 +0100

    Initial Memory Management Functions preparation

--- src/THNETII.WinApi.Native.MemoryManagement/MemoryManagementFunctions.cs
+++ src/THNETII.WinApi.Native.MemoryManagement/MemoryManagementFunctions.cs
@@ -1,5 +1,14 @@
-﻿namespace Microsoft.Win32.WinApi.SystemServices.MemoryManagement
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Win32.WinApi.SystemServices.MemoryManagement
 {
+    /// <summary>
+    /// Defines the memory management functions.
+    /// </summary>
+    /// <remarks>
+    /// <para>Original MSDN documentation page: <a href="https://msdn.microsoft.com/en-us/library/aa366781.aspx">Memory Management Functions</a></para>
+    /// </remarks>
     public static class MemoryManagementFunctions
     {
     }

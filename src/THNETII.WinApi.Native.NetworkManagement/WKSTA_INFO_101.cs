9d47fdff57b37b2e73e41ea4f07e7a4a1ae98c30
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-03-02 12:51:46 +0100

    WKSTA_INFO structures

---
+++ src/THNETII.WinApi.Native.NetworkManagement/WKSTA_INFO_101.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+
+using static Microsoft.Win32.WinApi.Networking.NetworkManagement.NetworkManagementFunctions;
+
+namespace Microsoft.Win32.WinApi.Networking.NetworkManagement
+{
+    /// <summary>
+    /// The <see cref="WKSTA_INFO_101"/> structure contains information about a workstation environment, including platform-specific information, the name of the domain and the local computer, and information concerning the operating system.
+    /// </summary>
+    /// <remarks>
+    /// <para>Original MSDN documentation page: <a href="https://msdn.microsoft.com/en-us/library/aa371403.aspx">WKSTA_INFO_101 structure</a></para>
+    /// </remarks>
+    /// <seealso cref="NetWkstaGetInfo"/>
+    /// <seealso cref="NetWkstaSetInfo"/>
+    [StructLayout(LayoutKind.Sequential)]
+    public class WKSTA_INFO_101
+    {
+        /// <summary>
+        /// The information level to use to retrieve platform-specific information. 
+        /// </summary>
+        [MarshalAs(UnmanagedType.I4)]
+        public PLATFORM_ID wki101_platform_id;
+        /// <summary>
+        /// A string specifying the name of the local computer.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string wki101_computername;
+        /// <summary>
+        /// A string specifying the name of the domain to which the computer belongs.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string wki101_langroup;
+        /// <summary>
+        /// The major version number of the operating system running on the computer.
+        /// </summary>
+        public int wki101_ver_major;
+        /// <summary>
+        /// The minor version number of the operating system running on the computer.
+        /// </summary>
+        public int wki101_ver_minor;
+        /// <summary>
+        /// A string that contains the path to the LANMAN directory.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string wki101_lanroot;
+    }
+}

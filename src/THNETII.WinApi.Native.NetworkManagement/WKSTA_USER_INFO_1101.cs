ece398351564e2a6e32b346271f74338d3498952
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-03-02 13:36:20 +0100

    WKSTA_USER_INFO structures

---
+++ src/THNETII.WinApi.Native.NetworkManagement/WKSTA_USER_INFO_1101.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+using static Microsoft.Win32.WinApi.Networking.NetworkManagement.NetworkManagementFunctions;
+
+namespace Microsoft.Win32.WinApi.Networking.NetworkManagement
+{
+    /// <summary>
+    /// The <see cref="WKSTA_USER_INFO_1101"/> structure contains information about the domains accessed by a workstation.
+    /// </summary>
+    /// <remarks>
+    /// <para>Original MSDN documentation page: <a href="https://msdn.microsoft.com/en-us/library/aa371410.aspx">WKSTA_USER_INFO_1101 structure</a></para>
+    /// </remarks>
+    /// <seealso cref="NetWkstaUserGetInfo"/>
+    /// <seealso cref="NetWkstaUserSetInfo"/>
+    [StructLayout(LayoutKind.Sequential)]
+    public class WKSTA_USER_INFO_1101
+    {
+        /// <summary>
+        /// Specifies the list of operating system domains browsed by the workstation. The domain names are separated by blanks.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string wkui1101_oth_domains;
+    }
+}

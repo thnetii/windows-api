c45e1933f6a890ab0bd60bbc34c9534d529e02cd
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-03-18 18:26:11 +0100

    NetUnjoinDomain function

--- src/THNETII.WinApi.Native.NetworkManagement/NetworkManagementFunctions.cs
+++ src/THNETII.WinApi.Native.NetworkManagement/NetworkManagementFunctions.cs
@@ -3442,6 +3442,45 @@ namespace Microsoft.Win32.WinApi.Networking.NetworkManagement
             [In] int Reserved
             );
         #endregion
+        #region NetUnjoinDomain function
+        /// <summary>
+        /// The <see cref="NetUnjoinDomain"/> function unjoins a computer from a workgroup or a domain.
+        /// </summary>
+        /// <param name="lpServer">A string that specifies the DNS or NetBIOS name of the computer on which the function is to execute. If this parameter is <c>null</c>, the local computer is used.</param>
+        /// <param name="lpAccount">A string that specifies the account name to use when connecting to the domain controller. The string must specify either a domain NetBIOS name and user account (for example, <c>REDMOND\user</c>) or the user principal name (UPN) of the user in the form of an Internet-style login name (for example, <c>"someone@example.com"</c>). If this parameter is <c>null</c>, the caller's context is used.</param>
+        /// <param name="lpPassword">If the <paramref name="lpAccount"/> parameter specifies an account name, this parameter must point to the password to use when connecting to the domain controller. Otherwise, this parameter must be <c>null</c>.</param>
+        /// <param name="fUnjoinOptions">Specifies the unjoin options. If this parameter is <see cref="NETSETUP_ACCT_DELETE"/>, the account is disabled when the unjoin occurs. Note that this option does not delete the account. Currently, there are no other unjoin options defined.</param>
+        /// <returns>
+        /// <para>If the function succeeds, the return value is <see cref="NERR_Success"/>.</para>
+        /// <para>
+        /// If the function fails, the return value can be one of the following error codes.
+        /// <list type="table">
+        /// <listheader><term>Return code</term><description>Description</description></listheader>
+        /// <term><see cref="ERROR_INVALID_PARAMETER"/></term><description>A parameter is incorrect.</description>
+        /// <term><see cref="NERR_SetupNotJoined"/></term><description>The computer is not currently joined to a domain.</description>
+        /// <term><see cref="NERR_SetupDomainController"/></term><description>This computer is a domain controller and cannot be unjoined from a domain.</description>
+        /// </list>
+        /// </para>
+        /// </returns>
+        /// <remarks>
+        /// <para>Unjoining (and joining) a computer to a domain or workgroup can be performed only by a member of the Administrators local group on the target computer. If you call the <see cref="NetUnjoinDomain"/> function remotely, you must supply credentials because you cannot delegate credentials under these circumstances.</para>
+        /// <para>Different processes, or different threads of the same process, should not call the <see cref="NetUnjoinDomain"/> function at the same time. This situation can leave the computer in an inconsistent state.</para>
+        /// <para>A system reboot is required after calling the <see cref="NetRenameMachineInDomain"/> function for the operation to complete.</para>
+        /// <para><strong>Minimum supported client</strong>: Windows 2000 Professional [desktop apps only]</para>
+        /// <para><strong>Minimum supported server</strong>: Windows 2000 Server [desktop apps only]</para>
+        /// <para>Original MSDN documentation page: <a href="https://msdn.microsoft.com/en-us/library/aa370644.aspx">NetUnjoinDomain function</a></para>
+        /// </remarks>
+        /// <seealso cref="NetJoinDomain"/>
+        /// <seealso cref="NetRenameMachineInDomain"/>
+        [DllImport("Netapi32.dll", CallingConvention = CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern Win32ErrorCode NetUnjoinDomain(
+            [In, MarshalAs(UnmanagedType.LPWStr)] string lpServer,
+            [In, MarshalAs(UnmanagedType.LPWStr)] string lpAccount,
+            [In, MarshalAs(UnmanagedType.LPWStr)] string lpPassword,
+            [In, MarshalAs(UnmanagedType.I4)] NETSETUP_OPTIONS fUnjoinOptions
+            );
+        #endregion
     }
 }
 

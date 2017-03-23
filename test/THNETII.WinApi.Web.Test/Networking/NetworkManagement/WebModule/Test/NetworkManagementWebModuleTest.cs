8d7407ba8eca1a670248b3344fb51220876d51b0
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-03-23 19:27:53 +0100

    Added THNETII.WinApi Web Testing project
    Also amended namespaces in the web projects

---
+++ test/THNETII.WinApi.Web.Test/Networking/NetworkManagement/WebModule/Test/NetworkManagementWebModuleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace THNETII.WinApi.Networking.NetworkManagement.WebModule.Test
+{
+    public class NetworkManagementWebModuleTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}

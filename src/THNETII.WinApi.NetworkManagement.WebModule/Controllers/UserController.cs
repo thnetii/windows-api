3d50e0425e1c02ab5fb9d2878d3bc86f0dbb080b
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-03-23 17:50:03 +0100

    Introduced NetworkManagement WebModule project

---
+++ src/THNETII.WinApi.NetworkManagement.WebModule/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace THNETII.WinApi.NetworkManagement.WebModule.Controllers
+{
+    [Controller]
+    [Area(NetworkManagementWebModule.GuidString)]
+    [Route("[controller]/[action]")]
+    public class UserController
+    {
+    }
+}
---
+++ src/THNETII.WinApi.NetworkManagement.WebModule/NetworkManagementWebModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace THNETII.WinApi.NetworkManagement.WebModule
+{
+    public class NetworkManagementWebModule
+    {
+        public const string GuidString = @"A2C9AC12-2EC5-49B1-A7D0-9729F4645F25";
+        public static readonly Guid Guid = new Guid(GuidString);
+    }
+}

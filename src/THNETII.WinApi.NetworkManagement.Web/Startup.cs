2745818dce71711222b1d8fe4262b4db02852d76
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-09-12 15:49:19 +0200

    Update Build system

--- src/THNETII.WinApi.NetworkManagement.Web/Startup.cs
+++ src/THNETII.WinApi.NetworkManagement.Web/Startup.cs
@@ -28,7 +28,8 @@ namespace THNETII.WinApi.NetworkManagement.Web
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc()
+                ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

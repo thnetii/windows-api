60caf988ed78a38173250d5e76b186a8a94c9be8
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 16:11:38 +0200

    DS_REPSYNCALL_ERROR Enumeration

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -4,12 +4,9 @@
 
 int main(int argc, char* argv[])
 {
-    const int size = sizeof(DS_NAME_FORMAT);
+    DS_REPSYNCALL_ERROR instance;
+    const int size = sizeof(instance);
     const int value = DS_NAME_ERROR_NO_SYNTACTICAL_MAPPING;
-
-    DS_NAME_FORMAT instance;
-
-    UNREFERENCED_PARAMETER(instance);
 
     return EXIT_SUCCESS;
 }

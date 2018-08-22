777f1fafa77b291c86c922ed5eeca59f0e6e3653
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 16:06:32 +0200

    DS_NAME_FORMAT Enumeration

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char* argv[])
 {
-    const int size = sizeof(DS_NAME_FLAGS);
+    const int size = sizeof(DS_NAME_FORMAT);
     const int value = DS_NAME_ERROR_NO_SYNTACTICAL_MAPPING;
 
-    DS_NAME_FLAGS instance;
+    DS_NAME_FORMAT instance;
 
     UNREFERENCED_PARAMETER(instance);
 

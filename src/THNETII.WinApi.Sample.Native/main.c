f101b4d7cc4cb186c7caaa06de56641fc65b3849
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 17:35:34 +0200

    _ADSPROPERROR structure

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -1,13 +1,12 @@
 #include <Windows.h>
 
-#include <NtDsAPI.h>
-#include <DSRole.h>
+#include <AdsProp.h>
 
 int main(int argc, char* argv[])
 {
-    DS_MANGLE_FOR instance;
+    ADSPROPERROR instance;
     const int size = sizeof(instance);
-    const int value = DS_NAME_ERROR_NO_SYNTACTICAL_MAPPING;
+    const int value = 0;
 
     return EXIT_SUCCESS;
 }

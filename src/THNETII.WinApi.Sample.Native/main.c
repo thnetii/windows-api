de2ee6aec2d22fe6b082b116dbd7ee5bd00bc02d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 13:39:23 +0200

    _DS_REPL_CURSOR_3W structure

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char* argv[])
 {
-    const int size = sizeof(DS_REPL_CURSORS_3W);
+    const int size = sizeof(DS_REPL_CURSOR_3W);
     const int value = DS_REPSYNC_ABANDONED;
 
-    DS_REPL_CURSORS_3W instance;
+    DS_REPL_CURSOR_3W instance;
 
     return EXIT_SUCCESS;
 }

f0d638739836420cbde584e09221240e5da8f09d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 20:47:06 +0200

    _ADS_OCTET_LIST structure

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-    ADS_CASEIGNORE_LIST instance;
+    ADS_OCTET_LIST instance;
     const int size = sizeof(instance);
     const int value = ADSTYPE_BACKLINK;
 

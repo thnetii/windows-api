55cafb0641c3ff8b89fcd9f785e8192198d42f89
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 16:43:14 +0200

    _DSROLE_PRIMARY_DOMAIN_INFO_LEVEL Enumeration

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-    DSROLE_OPERATION_STATE instance;
+    DSROLE_PRIMARY_DOMAIN_INFO_LEVEL instance;
     const int size = sizeof(instance);
     const int value = DS_NAME_ERROR_NO_SYNTACTICAL_MAPPING;
 

04af3c1361d4b53e57b5bd01cb9942bf404e8c4e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 10:31:20 +0200

    _DS_REPL_ATTR_META_DATA structure

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char* argv[])
 {
-    const int size = sizeof(DS_REPL_OBJ_META_DATA);
+    const int size = sizeof(DS_REPL_ATTR_META_DATA);
     const int value = DS_REPL_INFO_TYPE_MAX;
 
-    DS_REPL_OBJ_META_DATA instance;
+    DS_REPL_ATTR_META_DATA instance;
 
     return EXIT_SUCCESS;
 }

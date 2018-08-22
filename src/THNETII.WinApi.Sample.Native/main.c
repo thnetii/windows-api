61ec5f0ebded8f639dddfa2bf60d7822e1cfc58a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 13:10:11 +0200

    _DS_REPL_VALUE_META_DATA structure

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char* argv[])
 {
-    const int size = sizeof(DS_REPL_ATTR_VALUE_META_DATA);
+    const int size = sizeof(DS_REPL_VALUE_META_DATA);
     const int value = DS_REPSYNC_ABANDONED;
 
-    DS_REPL_ATTR_VALUE_META_DATA instance;
+    DS_REPL_VALUE_META_DATA instance;
 
     return EXIT_SUCCESS;
 }

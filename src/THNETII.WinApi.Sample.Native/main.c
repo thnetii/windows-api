28ed80d81d0bf4421ba06595e658c43da124f326
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 16:27:17 +0200

    DS_SPN_WRITE_OP Enumeration

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-    DS_SPN_NAME_TYPE instance;
+    DS_SPN_WRITE_OP instance;
     const int size = sizeof(instance);
     const int value = DS_NAME_ERROR_NO_SYNTACTICAL_MAPPING;
 

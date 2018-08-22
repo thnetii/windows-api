1384e350763dd7ead922b5703b1e0d7544536668
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-22 15:53:04 +0200

    DS_NAME_FLAGS Enumeration

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char* argv[])
 {
-    const int size = sizeof(DS_NAME_ERROR);
-    const int value = DS_REPSYNC_ABANDONED;
+    const int size = sizeof(DS_NAME_FLAGS);
+    const int value = DS_NAME_ERROR_NO_SYNTACTICAL_MAPPING;
 
-    DS_NAME_ERROR instance;
+    DS_NAME_FLAGS instance;
 
     UNREFERENCED_PARAMETER(instance);
 

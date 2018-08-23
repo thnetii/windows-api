451e93d72ea37007c478ad329a0e9634f0fe93e3
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-23 14:52:32 +0200

    ADS_DN_WITH_BINARY structure

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-    ADS_DN_WITH_STRING instance;
+    ADS_DN_WITH_BINARY instance;
     const int size = sizeof(instance);
     const int value = ADSTYPE_BACKLINK;
 

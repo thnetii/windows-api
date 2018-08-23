d7b7d19d6008a7a8b6913caa717fe69a6858b9b2
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-23 02:30:01 +0200

    ADS_TYPEDNAME structure

---
+++ src/THNETII.WinApi.Native.SecurityIdentity.Iads/ADS_TYPEDNAME.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using THNETII.InteropServices.NativeMemory;
+
+namespace THNETII.WinApi.Native.SecurityIdentity.Iads
+{
+    /// <summary>
+    /// The <see cref="ADS_TYPEDNAME "/> structure is an ADSI representation of the <strong>Typed Name</strong> attribute syntax.
+    /// </summary>
+    /// <remarks>
+    /// <para>Original MSDN documentation page: <a href="https://docs.microsoft.com/en-us/windows/desktop/api/iads/ns-iads-__midl___midl_itf_ads_0000_0000_0009">__MIDL___MIDL_itf_ads_0000_0000_0009 structure</a></para>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ADS_TYPEDNAME
+    {
+        /// <summary>
+        /// A pointer to a null-terminated Unicode string that contains an object name.
+        /// </summary>
+        public IntPtr pwzObjectName;
+
+        /// <summary>
+        /// Gets the span over the string that contains an object name.
+        /// </summary>
+        public Span<char> ObjectName => pwzObjectName.AsZeroTerminatedUnicodeSpan();
+
+        /// <summary>
+        /// The priority associated with the object.
+        /// </summary>
+        public int Level;
+
+        /// <summary>
+        /// The frequency of reference of the object.
+        /// </summary>
+        public int Interval;
+    }
+}
--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-    ADS_BACKLINK instance;
+    ADS_TYPEDNAME instance;
     const int size = sizeof(instance);
     const int value = ADSTYPE_BACKLINK;
 

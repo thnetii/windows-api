3214948b1d4f77842be8f8a3c3d9960deb3bcc7d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-23 03:03:08 +0200

    ADS_FAXNUMBER structure

---
+++ src/THNETII.WinApi.Native.SecurityIdentity.Iads/ADS_FAXNUMBER.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using THNETII.InteropServices.NativeMemory;
+
+namespace THNETII.WinApi.Native.SecurityIdentity.Iads
+{
+    /// <summary>
+    /// The <see cref="ADS_FAXNUMBER"/> structure is an ADSI representation of the <strong>Facsimile Telephone</strong> attribute syntax.
+    /// </summary>
+    /// <remarks>
+    /// <para>Original MSDN documentation page: <a href="https://docs.microsoft.com/en-us/windows/desktop/api/iads/ns-iads-__midl___midl_itf_ads_0000_0000_0013">__MIDL___MIDL_itf_ads_0000_0000_0013 structure</a></para>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ADS_FAXNUMBER
+    {
+        /// <summary>
+        /// Pointer to a null-terminated Unicode string value that contains the telephone number of the facsimile (fax) machine.
+        /// </summary>
+        public IntPtr pwzTelephoneNumber;
+
+        /// <summary>
+        /// Gets a span over the string value that contains the telephone number of the facsimile (fax) machine.
+        /// </summary>
+        public Span<char> TelephoneNumber => pwzTelephoneNumber.AsZeroTerminatedUnicodeSpan();
+
+        /// <summary>
+        /// The number of data bits.
+        /// </summary>
+        public int NumberOfBits;
+
+        /// <summary>
+        /// Pointer to optional parameters for the fax machine.
+        /// </summary>
+        public IntPtr pParamaters;
+
+        /// <summary>
+        /// Gets a span over the optional parameters for the fax machine.
+        /// </summary>
+        public unsafe Span<byte> Parameters =>
+            new Span<byte>(pParamaters.ToPointer(), NumberOfBits);
+    }
+}
--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-    ADS_REPLICAPOINTER instance;
+    ADS_FAXNUMBER instance;
     const int size = sizeof(instance);
     const int value = ADSTYPE_BACKLINK;
 

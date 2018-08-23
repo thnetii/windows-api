b8ce2b4fde2de4afc314d1fec029111d45c90141
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-23 15:30:16 +0200

    ADS_ATTR_INFO structure

---
+++ src/THNETII.WinApi.Native.SecurityIdentity.Iads/ADS_ATTR_TYPE.cs
@@ -0,0 +1,31 @@
+﻿namespace THNETII.WinApi.Native.SecurityIdentity.Iads
+{
+    /// <summary>
+    /// The following constants are used with the <see cref="ADS_ATTR_INFO.dwControlCode"/> member of <see cref="ADS_ATTR_INFO"/> structure to specify the type of operation to be performed when an attribute is modified with the <see cref="IDirectoryObject.SetObjectAttributes"/> method. For more information about using these values, see <a href="https://docs.microsoft.com/nb-no/windows/desktop/ADSI/modifying-attributes-with-adsi">Modifying Attributes with ADSI</a>.
+    /// </summary>
+    /// <remarks>
+    /// These constants are intended to be used with the <see cref="ADS_ATTR_INFO"/> structure in the <see cref="IDirectoryObject.SetObjectAttributes"/> method. These constants should not be confused with members of the <see cref="ADS_PROPERTY_OPERATION_ENUM"/> enumeration, which are intended to be used with the <see cref="IADs.PutEx"/> method.
+    /// <para>Original MSDN documentation page: <a href="https://docs.microsoft.com/en-us/windows/desktop/ADSI/adsi-attribute-modification-types">ADSI Attribute Modification Types</a></para>
+    /// </remarks>
+    /// <seealso cref="ADS_ATTR_INFO"/>
+    /// <seealso cref="IDirectoryObject.SetObjectAttributes"/>
+    public enum ADS_ATTR_TYPE : int
+    {
+        /// <summary>
+        /// Causes all attribute values to be removed from an object.
+        /// </summary>
+        ADS_ATTR_CLEAR = 1,
+        /// <summary>
+        /// Causes the specified attribute values to be updated.
+        /// </summary>
+        ADS_ATTR_UPDATE = 2,
+        /// <summary>
+        /// Causes the specified attribute values to be appended to the existing attribute values.
+        /// </summary>
+        ADS_ATTR_APPEND = 3,
+        /// <summary>
+        /// Causes the specified attribute values to be removed from an object.
+        /// </summary>
+        ADS_ATTR_DELETE = 4
+    }
+}
--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-    ADSVALUE instance;
+    ADS_ATTR_INFO instance;
     const int size = sizeof(instance);
     const int value = ADSTYPE_BACKLINK;
 

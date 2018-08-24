055060560aa48dfcc8d7d65e9dad56bc6807b161
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-24 04:07:26 +0200

    AMSI_RESULT Enumeration

--- src/THNETII.WinApi.Sample.Native/main.c
+++ src/THNETII.WinApi.Sample.Native/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-    AMSI_ATTRIBUTE instance;
+    AMSI_RESULT instance;
     const int size = sizeof(instance);
     const int value = AMSI_ATTRIBUTE_APP_NAME;
 

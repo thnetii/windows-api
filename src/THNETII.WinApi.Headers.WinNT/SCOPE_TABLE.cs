6d763f36e1f73092f3d173cb631714d80a0fdc7a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-25 15:42:50 +0200

    SCOPE_TABLE has same structure across all architectures

--- src/THNETII.WinApi.Headers.WinNT/SCOPE_TABLE.cs
+++ src/THNETII.WinApi.Headers.WinNT/SCOPE_TABLE.cs
@@ -1,21 +1,23 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
+using THNETII.InteropServices.NativeMemory;
+using static THNETII.WinApi.Native.WinNT.WinNTConstants;
 
 namespace THNETII.WinApi.Native.WinNT
 {
-    using System;
-    using THNETII.InteropServices.NativeMemory;
-    using static WinNTConstants;
-
+    /// <summary>
+    /// Scope table structure definition.
+    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
-    public struct SCOPE_TABLE_AMD64
+    public struct SCOPE_TABLE
     {
         public int Count;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = ANYSIZE_ARRAY)]
-        public SCOPE_RECORD_AMD64[] ScopeRecord;
+        public SCOPE_RECORD[] ScopeRecord;
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    public struct SCOPE_RECORD_AMD64
+    public struct SCOPE_RECORD
     {
         public int BeginAddress;
         public int EndAddress;
@@ -23,15 +25,15 @@ namespace THNETII.WinApi.Native.WinNT
         public int JumpTarget;
     }
 
-    public class SCOPE_TABLE_AMD64_MARSHAL
+    public class SCOPE_TABLE_MARSHAL
 #if !NETSTANDARD1_3
         : ICustomMarshaler
 #endif // !NETSTANDARD1_3
     {
         private static readonly int offsetScopeRecord =
-            Marshal.OffsetOf<SCOPE_TABLE_AMD64>(nameof(SCOPE_TABLE_AMD64.ScopeRecord)).ToInt32();
+            Marshal.OffsetOf<SCOPE_TABLE>(nameof(SCOPE_TABLE.ScopeRecord)).ToInt32();
         private static readonly int offsetScopeRecordPlusOne =
-            offsetScopeRecord + SizeOf<SCOPE_RECORD_AMD64>.Bytes * 1;
+            offsetScopeRecord + SizeOf<SCOPE_RECORD>.Bytes * 1;
 
         public void CleanUpManagedData(object ManagedObj)
         {
@@ -51,19 +53,19 @@ namespace THNETII.WinApi.Native.WinNT
 
         public IntPtr MarshalManagedToNative(object ManagedObj)
         {
-            if (!(ManagedObj is SCOPE_TABLE_AMD64 scopeTable))
+            if (!(ManagedObj is SCOPE_TABLE scopeTable))
                 return IntPtr.Zero;
 
-            int cbScopeTable = SizeOf<SCOPE_TABLE_AMD64>.Bytes;
+            int cbScopeTable = SizeOf<SCOPE_TABLE>.Bytes;
             int additionalRecords = ((scopeTable.ScopeRecord?.Length ?? 0) > ANYSIZE_ARRAY)
                 ? scopeTable.ScopeRecord.Length - ANYSIZE_ARRAY
                 : 0;
-            cbScopeTable += additionalRecords * SizeOf<SCOPE_RECORD_AMD64>.Bytes;
+            cbScopeTable += additionalRecords * SizeOf<SCOPE_RECORD>.Bytes;
 
             var pScopeTable = Marshal.AllocCoTaskMem(cbScopeTable);
             Marshal.StructureToPtr(scopeTable, pScopeTable, fDeleteOld: false);
             IntPtr pRecord = pScopeTable + offsetScopeRecordPlusOne;
-            for (int i = 1; i < scopeTable.ScopeRecord.Length; i++, pRecord += SizeOf<SCOPE_RECORD_AMD64>.Bytes)
+            for (int i = 1; i < scopeTable.ScopeRecord.Length; i++, pRecord += SizeOf<SCOPE_RECORD>.Bytes)
                 Marshal.StructureToPtr(scopeTable.ScopeRecord[i], pRecord, fDeleteOld: false);
             return pScopeTable;
         }
@@ -71,23 +73,23 @@ namespace THNETII.WinApi.Native.WinNT
         public object MarshalNativeToManaged(IntPtr pNativeData)
         {
             if (pNativeData == IntPtr.Zero)
-                return default(SCOPE_TABLE_AMD64);
+                return default(SCOPE_TABLE);
 
-            var scopeTable = Marshal.PtrToStructure<SCOPE_TABLE_AMD64>(pNativeData);
+            var scopeTable = Marshal.PtrToStructure<SCOPE_TABLE>(pNativeData);
             if (scopeTable.Count < 2)
                 return scopeTable;
-            var records = new SCOPE_RECORD_AMD64[scopeTable.Count];
+            var records = new SCOPE_RECORD[scopeTable.Count];
             IntPtr pRecord = pNativeData + offsetScopeRecord;
-            for (int i = 0; i < scopeTable.Count; i++, pRecord += SizeOf<SCOPE_RECORD_AMD64>.Bytes)
-                records[i] = Marshal.PtrToStructure<SCOPE_RECORD_AMD64>(pRecord);
+            for (int i = 0; i < scopeTable.Count; i++, pRecord += SizeOf<SCOPE_RECORD>.Bytes)
+                records[i] = Marshal.PtrToStructure<SCOPE_RECORD>(pRecord);
             scopeTable.ScopeRecord = records;
             return scopeTable;
         }
 
-        public static SCOPE_TABLE_AMD64_MARSHAL GetInstance() =>
-            new SCOPE_TABLE_AMD64_MARSHAL();
+        public static SCOPE_TABLE_MARSHAL GetInstance() =>
+            new SCOPE_TABLE_MARSHAL();
 
-        public static SCOPE_TABLE_AMD64_MARSHAL GetInstance(string cookie) =>
-            new SCOPE_TABLE_AMD64_MARSHAL();
+        public static SCOPE_TABLE_MARSHAL GetInstance(string cookie) =>
+            new SCOPE_TABLE_MARSHAL();
     }
 }

86e6d23b943e5e4eef6a1e9a0b08fd4fcb2cbd68
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-02 02:36:08 +0100

    NT Defined Privileges

--- src/THNETII.WinApi.Constants.WinNT/WinNTConstants.cs
+++ src/THNETII.WinApi.Constants.WinNT/WinNTConstants.cs
@@ -1883,5 +1883,59 @@ namespace THNETII.WinApi.Native.WinNT
         public const int SE_SECURITY_DESCRIPTOR_FLAG_NO_ACCESS_FILTER_ACE = 0x00000004;
         public const int SE_SECURITY_DESCRIPTOR_VALID_FLAGS = 0x00000007;
 
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 10482
+        ////////////////////////////////////////////////////////////////////////
+        //                                                                    //
+        //               NT Defined Privileges                                //
+        //                                                                    //
+        ////////////////////////////////////////////////////////////////////////
+
+        public const string SE_CREATE_TOKEN_NAME = "SeCreateTokenPrivilege";
+        public const string SE_ASSIGNPRIMARYTOKEN_NAME = "SeAssignPrimaryTokenPrivilege";
+        public const string SE_LOCK_MEMORY_NAME = "SeLockMemoryPrivilege";
+        public const string SE_INCREASE_QUOTA_NAME = "SeIncreaseQuotaPrivilege";
+        public const string SE_UNSOLICITED_INPUT_NAME = "SeUnsolicitedInputPrivilege";
+        public const string SE_MACHINE_ACCOUNT_NAME = "SeMachineAccountPrivilege";
+        public const string SE_TCB_NAME = "SeTcbPrivilege";
+        public const string SE_SECURITY_NAME = "SeSecurityPrivilege";
+        public const string SE_TAKE_OWNERSHIP_NAME = "SeTakeOwnershipPrivilege";
+        public const string SE_LOAD_DRIVER_NAME = "SeLoadDriverPrivilege";
+        public const string SE_SYSTEM_PROFILE_NAME = "SeSystemProfilePrivilege";
+        public const string SE_SYSTEMTIME_NAME = "SeSystemtimePrivilege";
+        public const string SE_PROF_SINGLE_PROCESS_NAME = "SeProfileSingleProcessPrivilege";
+        public const string SE_INC_BASE_PRIORITY_NAME = "SeIncreaseBasePriorityPrivilege";
+        public const string SE_CREATE_PAGEFILE_NAME = "SeCreatePagefilePrivilege";
+        public const string SE_CREATE_PERMANENT_NAME = "SeCreatePermanentPrivilege";
+        public const string SE_BACKUP_NAME = "SeBackupPrivilege";
+        public const string SE_RESTORE_NAME = "SeRestorePrivilege";
+        public const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
+        public const string SE_DEBUG_NAME = "SeDebugPrivilege";
+        public const string SE_AUDIT_NAME = "SeAuditPrivilege";
+        public const string SE_SYSTEM_ENVIRONMENT_NAME = "SeSystemEnvironmentPrivilege";
+        public const string SE_CHANGE_NOTIFY_NAME = "SeChangeNotifyPrivilege";
+        public const string SE_REMOTE_SHUTDOWN_NAME = "SeRemoteShutdownPrivilege";
+        public const string SE_UNDOCK_NAME = "SeUndockPrivilege";
+        public const string SE_SYNC_AGENT_NAME = "SeSyncAgentPrivilege";
+        public const string SE_ENABLE_DELEGATION_NAME = "SeEnableDelegationPrivilege";
+        public const string SE_MANAGE_VOLUME_NAME = "SeManageVolumePrivilege";
+        public const string SE_IMPERSONATE_NAME = "SeImpersonatePrivilege";
+        public const string SE_CREATE_GLOBAL_NAME = "SeCreateGlobalPrivilege";
+        public const string SE_TRUSTED_CREDMAN_ACCESS_NAME = "SeTrustedCredManAccessPrivilege";
+        public const string SE_RELABEL_NAME = "SeRelabelPrivilege";
+        public const string SE_INC_WORKING_SET_NAME = "SeIncreaseWorkingSetPrivilege";
+        public const string SE_TIME_ZONE_NAME = "SeTimeZonePrivilege";
+        public const string SE_CREATE_SYMBOLIC_LINK_NAME = "SeCreateSymbolicLinkPrivilege";
+        public const string SE_DELEGATE_SESSION_USER_IMPERSONATE_NAME = "SeDelegateSessionUserImpersonatePrivilege";
+
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 10527
+        //
+        // List Of String Capabilities.
+        //
+        public const string SE_ACTIVATE_AS_USER_CAPABILITY = "activateAsUser";
+        public const string SE_CONSTRAINED_IMPERSONATION_CAPABILITY = "constrainedImpersonation";
+        public const string SE_SESSION_IMPERSONATION_CAPABILITY = "sessionImpersonation";
+        public const string SE_MUMA_CAPABILITY = "muma";
+        public const string SE_DEVELOPMENT_MODE_NETWORK_CAPABILITY = "developmentModeNetwork";
+
     }
 }

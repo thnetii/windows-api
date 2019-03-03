179235c03173c2c8bec59d32237c34ae0d213366
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-03 19:19:18 +0100

    TOKEN_SID_INFORMATION structure

---
+++ src/THNETII.WinApi.Headers.WinNT/TOKEN_SID_INFORMATION.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 10899
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct TOKEN_SID_INFORMATION
+    {
+        public SID* Sid;
+    }
+}

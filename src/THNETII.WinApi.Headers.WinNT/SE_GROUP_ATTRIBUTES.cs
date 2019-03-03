edcbf7b5371d3a9c669b340fe6be549491ccd028
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-03 11:50:24 +0100

    TOKEN_GROUPS_AND_PRIVILEGES structure

--- src/THNETII.WinApi.Headers.WinNT/SE_GROUP_ATTRIBUTES.cs
+++ src/THNETII.WinApi.Headers.WinNT/SE_GROUP_ATTRIBUTES.cs
@@ -9,14 +9,45 @@ namespace THNETII.WinApi.Native.WinNT
     [Flags]
     public enum SE_GROUP_ATTRIBUTES
     {
+        /// <summary>
+        /// The SID cannot have the <see cref="SE_GROUP_ENABLED"/> attribute cleared by a call to the <see cref="AdjustTokenGroups"/> function. However, you can use the <see cref="CreateRestrictedToken"/> function to convert a mandatory SID to a deny-only SID.
+        /// </summary>
         SE_GROUP_MANDATORY = WinNTConstants.SE_GROUP_MANDATORY,
+        /// <summary>
+        /// The SID is enabled by default.
+        /// </summary>
         SE_GROUP_ENABLED_BY_DEFAULT = WinNTConstants.SE_GROUP_ENABLED_BY_DEFAULT,
+        /// <summary>
+        /// <para> The <see cref="SID"/> is enabled for access checks. When the system performs an access check, it checks for access-allowed and access-denied <a href="https://msdn.microsoft.com/0baaa937-f635-4500-8dcd-9dbbd6f4cd02">access control entries</a> (ACEs) that apply to the SID.</para>
+        /// <para>A SID without this attribute is ignored during an access check unless the <see cref="SE_GROUP_USE_FOR_DENY_ONLY"/> attribute is set.</para>
+        /// </summary>
         SE_GROUP_ENABLED = WinNTConstants.SE_GROUP_ENABLED,
+        /// <summary>
+        /// The SID identifies a group account for which the user of the token is the owner of the group, or the SID can be assigned as the owner of the token or objects.
+        /// </summary>
         SE_GROUP_OWNER = WinNTConstants.SE_GROUP_OWNER,
+        /// <summary>
+        /// The SID is a deny-only SID in a <a href="https://msdn.microsoft.com/06580ab9-ff58-4aa9-bf88-9536a2e1981d">restricted token</a>. When the system performs an access check, it checks for access-denied ACEs that apply to the SID; it ignores access-allowed ACEs for the SID.
+        /// <para>If this attribute is set, <see cref="SE_GROUP_ENABLED"/> is not set, and the SID cannot be reenabled.</para>
+        /// </summary>
         SE_GROUP_USE_FOR_DENY_ONLY = WinNTConstants.SE_GROUP_USE_FOR_DENY_ONLY,
+        /// <summary>
+        /// The SID is a mandatory integrity SID.
+        /// <para><strong>Windows Server 2008, Windows Vista, Windows Server 2003 and Windows XP</strong>: This value is not supported.</para>
+        /// </summary>
         SE_GROUP_INTEGRITY = WinNTConstants.SE_GROUP_INTEGRITY,
+        /// <summary>
+        /// The mandatory integrity SID is evaluated during access check.
+        /// <para><strong>Windows Server 2008, Windows Vista, Windows Server 2003 and Windows XP</strong>: This value is not supported.</para>
+        /// </summary>
         SE_GROUP_INTEGRITY_ENABLED = WinNTConstants.SE_GROUP_INTEGRITY_ENABLED,
+        /// <summary>
+        /// The SID is a logon SID that identifies the <a href="https://msdn.microsoft.com/65dd9a04-fc7c-4179-95ff-dac7dad4668f">logon session</a> associated with an access token.
+        /// </summary>
         SE_GROUP_LOGON_ID = WinNTConstants.SE_GROUP_LOGON_ID,
+        /// <summary>
+        ///  The SID identifies a domain-local group.
+        /// </summary>
         SE_GROUP_RESOURCE = WinNTConstants.SE_GROUP_RESOURCE,
         SE_GROUP_VALID_ATTRIBUTES = WinNTConstants.SE_GROUP_VALID_ATTRIBUTES,
     }
---
+++ src/THNETII.WinApi.Headers.WinNT/TOKEN_GROUPS_AND_PRIVILEGES.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using THNETII.InteropServices.NativeMemory;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 10778
+    /// <summary>
+    /// The <see cref="TOKEN_GROUPS_AND_PRIVILEGES"/> structure contains information about the group <a href="https://msdn.microsoft.com/3e9d7672-2314-45c8-8178-5a0afcfd0c50">security identifiers</a> (SIDs) in an <a href="https://msdn.microsoft.com/0baaa937-f635-4500-8dcd-9dbbd6f4cd02">access token</a>.
+    /// </summary>
+    /// <remarks>
+    /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/desktop/api/winnt/ns-winnt-token_groups_and_privileges">TOKEN_GROUPS_AND_PRIVILEGES structure</a></para>
+    /// </remarks>
+    /// <seealso cref="AdjustTokenGroups"/>
+    /// <seealso cref="CreateRestrictedToken"/>
+    /// <seealso cref="SID_AND_ATTRIBUTES"/>
+    /// <seealso cref="TOKEN_CONTROL"/>
+    /// <seealso cref="TOKEN_DEFAULT_DACL"/>
+    /// <seealso cref="TOKEN_INFORMATION_CLASS"/>
+    /// <seealso cref="TOKEN_OWNER"/>
+    /// <seealso cref="TOKEN_PRIMARY_GROUP"/>
+    /// <seealso cref="TOKEN_PRIVILEGES"/>
+    /// <seealso cref="TOKEN_SOURCE"/>
+    /// <seealso cref="TOKEN_STATISTICS"/>
+    /// <seealso cref="TOKEN_TYPE"/>
+    /// <seealso cref="TOKEN_USER"/>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TOKEN_GROUPS_AND_PRIVILEGES
+    {
+        /// <summary>
+        /// Number of SIDs in the access token.
+        /// </summary>
+        public int SidCount;
+        /// <summary>
+        /// Length, in bytes, required to hold all of the user SIDs and the account SID for the group.
+        /// </summary>
+        public int SidLength;
+        /// <summary>
+        /// A pointer to an array of <see cref="SID_AND_ATTRIBUTES"/> structures that contain a set of SIDs and corresponding attributes.
+        /// <para>Use the <see cref="Sids"/> property to access the SIDs.</para>
+        /// </summary>
+        public IntPtr SidsPtr;
+        /// <summary>
+        /// A span of <see cref="SID_AND_ATTRIBUTES"/> structures that contain a set of SIDs and corresponding attributes.
+        /// <para>The possible values for the <see cref="SID_AND_ATTRIBUTES.Attributes"/> members of the <see cref="SID_AND_ATTRIBUTES"/> structures are defined by the <see cref="SE_GROUP_ATTRIBUTES"/> enumeration.</para>
+        /// </summary>
+        public Span<SID_AND_ATTRIBUTES> Sids => SidsPtr.AsRefStructSpan<SID_AND_ATTRIBUTES>(SidCount);
+        /// <summary>
+        /// Number of restricted SIDs.
+        /// </summary>
+        public int RestrictedSidCount;
+        /// <summary>
+        /// Length, in bytes, required to hold all of the restricted SIDs.
+        /// </summary>
+        public int RestrictedSidLength;
+        /// <summary>
+        /// A pointer to an array of <see cref="SID_AND_ATTRIBUTES"/> structures that contain a set of restricted SIDs and corresponding attributes.
+        /// <para>Use the <see cref="RestrictedSids"/> property to access the SIDs.</para>
+        /// </summary>
+        public IntPtr RestrictedSidsPtr;
+        /// <summary>
+        /// A span of <see cref="SID_AND_ATTRIBUTES"/> structures that contain a set of restricted SIDs and corresponding attributes.
+        /// <para>The possible values for the <see cref="SID_AND_ATTRIBUTES.Attributes"/> members of the <see cref="SID_AND_ATTRIBUTES"/> structures are defined by the <see cref="SE_GROUP_ATTRIBUTES"/> enumeration.</para>
+        /// </summary>
+        public Span<SID_AND_ATTRIBUTES> RestrictedSids => RestrictedSidsPtr.AsRefStructSpan<SID_AND_ATTRIBUTES>(RestrictedSidCount);
+        /// <summary>
+        /// Number of privileges.
+        /// </summary>
+        public int PrivilegeCount;
+        /// <summary>
+        /// Length, in bytes, needed to hold the privilege array.
+        /// </summary>
+        public int PrivilegeLength;
+        /// <summary>
+        /// A pointer to an array of <see cref="LUID_AND_ATTRIBUTES"/> structures containing the priviliges of the token.
+        /// <para>Use the <see cref="Privileges"/> property to access the privileges.</para>
+        /// </summary>
+        public IntPtr PrivilegesPtr;
+        /// <summary>
+        /// A span of <see cref="LUID_AND_ATTRIBUTES"/> structures containing the priviliges of the token.
+        /// </summary>
+        public Span<LUID_AND_ATTRIBUTES> Privileges => PrivilegesPtr.AsRefStructSpan<LUID_AND_ATTRIBUTES>(PrivilegeCount);
+        /// <summary>
+        /// <a href="https://msdn.microsoft.com/65dd9a04-fc7c-4179-95ff-dac7dad4668f">Locally unique identifier</a> (LUID) of the authenticator of the token.
+        /// </summary>
+        public LUID AuthenticationId;
+    }
+}

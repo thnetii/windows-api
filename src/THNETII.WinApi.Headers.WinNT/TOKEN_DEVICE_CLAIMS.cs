b43d87c54e7b12228c208ecb5a0bde941cb1f518
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-03 11:27:18 +0100

    TOKEN_USER_CLAIMS and TOKEN_DEVICE_CLAIMS structures

---
+++ src/THNETII.WinApi.Headers.WinNT/TOKEN_DEVICE_CLAIMS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 10770
+    /// <summary>
+    /// The <see cref="TOKEN_DEVICE_CLAIMS"/> structure defines the device claims for the token.
+    /// </summary>
+    /// <remarks>
+    /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/desktop/api/winnt/ns-winnt-token_device_claims">TOKEN_DEVICE_CLAIMS structure</a></para>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TOKEN_DEVICE_CLAIMS
+    {
+        /// <summary>
+        /// The device claims for the token.
+        /// </summary>
+        public IntPtr DeviceClaims;
+    }
+}
---
+++ src/THNETII.WinApi.Headers.WinNT/TOKEN_USER_CLAIMS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 10770
+    /// <summary>
+    /// The <see cref="TOKEN_USER_CLAIMS"/> structure defines the user claims for the token.
+    /// </summary>
+    /// <remarks>
+    /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/desktop/api/winnt/ns-winnt-token_user_claims">TOKEN_USER_CLAIMS structure</a></para>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TOKEN_USER_CLAIMS
+    {
+        /// <summary>
+        /// The user claims for the token.
+        /// </summary>
+        public IntPtr UserClaims;
+    }
+}

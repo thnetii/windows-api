0de0f1e5047608f8b9cd8258433ce6ccb3655ee0
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-03 19:19:03 +0100

    TOKEN_APPCONTAINER_INFORMATION structure

---
+++ src/THNETII.WinApi.Headers.WinNT/TOKEN_APPCONTAINER_INFORMATION.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using THNETII.InteropServices.NativeMemory;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 10891
+    /// <summary>
+    /// The <see cref="TOKEN_APPCONTAINER_INFORMATION"/> structure specifies all the information in a token that is necessary for an app container.
+    /// </summary>
+    /// <remarks>
+    /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/desktop/api/winnt/ns-winnt-token_appcontainer_information">TOKEN_APPCONTAINER_INFORMATION structure</a></para>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct TOKEN_APPCONTAINER_INFORMATION
+    {
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 10896
+        public static readonly int TOKEN_APPCONTAINER_SID_MAX_SIZE = SizeOf<TOKEN_APPCONTAINER_INFORMATION>.Bytes + SID.SECURITY_MAX_SID_SIZE;
+
+        /// <summary>
+        /// The <a href="https://msdn.microsoft.com/3e9d7672-2314-45c8-8178-5a0afcfd0c50">security identifier</a> (SID) of the app container.
+        /// </summary>
+        public SID* TokenAppContainer;
+    }
+}

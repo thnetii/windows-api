9b6d0043348e3b4ab601ae6fa49ffec73a6f1f11
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-04 18:38:03 +0100

    SE_LEARNING_MODE_DATA_TYPE

---
+++ src/THNETII.WinApi.Headers.WinNT/SE_LEARNING_MODE_DATA_TYPE.cs
@@ -0,0 +1,13 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11243
+    /// <summary>
+    /// Learning Mode Types.
+    /// </summary>
+    public enum SE_LEARNING_MODE_DATA_TYPE
+    {
+        SeLearningModeInvalidType = 0,
+        SeLearningModeSettings,
+        SeLearningModeMax
+    }
+}

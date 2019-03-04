7760f4235ca670808abf028c6eb8c61b9bf589a6
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-04 15:47:30 +0100

    SE_IMPERSONATION_STATE

---
+++ src/THNETII.WinApi.Headers.WinNT/SE_IMPERSONATION_STATE.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Principal;
+using THNETII.InteropServices.NativeMemory;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11184
+    /// <summary>
+    /// Used to represent information related to a thread impersonation
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SE_IMPERSONATION_STATE
+    {
+        public static readonly int SizeOf = SizeOf<SE_IMPERSONATION_STATE>.Bytes;
+
+        /// <summary>
+        /// Specifies the size, in bytes, of this structure.
+        /// <para>Use the value of the static field <see cref="SizeOf"/>.</para>
+        /// </summary>
+        public int Length;
+        internal byte CopyOnOpenValue;
+        public bool CopyOnOpen
+        {
+            get => CopyOnOpenValue != 0;
+            set => CopyOnOpenValue = value ? (byte)1 : (byte)0;
+        }
+        internal byte EffectiveOnlyValue;
+        public bool EffectiveOnly
+        {
+            get => EffectiveOnlyValue != 0;
+            set => EffectiveOnlyValue = value ? (byte)1 : (byte)0;
+        }
+        public TokenImpersonationLevel Level;
+    }
+}

c8b600759d91aeb6a0acb93cc93c63d9713580d5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-04 18:37:54 +0100

    SE_IMAGE_SIGNATURE_TYPE

---
+++ src/THNETII.WinApi.Headers.WinNT/SE_IMAGE_SIGNATURE_TYPE.cs
@@ -0,0 +1,17 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11243
+    /// <summary>
+    /// Image signature types.
+    /// </summary>
+    public enum SE_IMAGE_SIGNATURE_TYPE
+    {
+        SeImageSignatureNone = 0,
+        SeImageSignatureEmbedded,
+        SeImageSignatureCache,
+        SeImageSignatureCatalogCached,
+        SeImageSignatureCatalogNotCached,
+        SeImageSignatureCatalogHint,
+        SeImageSignaturePackageCatalog,
+    }
+}

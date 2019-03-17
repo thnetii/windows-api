d2132f734c4d800586ce6d64678b1a1500b288a7
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-17 11:35:58 +0100

    JOB_SET_ARRAY

---
+++ src/THNETII.WinApi.Headers.WinNT/JOB_SET_ARRAY.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11337
+    [StructLayout(LayoutKind.Sequential)]
+    public struct JOB_SET_ARRAY
+    {
+        /// <summary>Handle to job object to insert</summary>
+        public IntPtr JobHandle;
+        /// <summary>Level of this job in the set. Must be > 0. Can be sparse.</summary>
+        public int MemberLevel;
+        /// <summary>Unused. Must be zero</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public int Flags;
+    }
+}

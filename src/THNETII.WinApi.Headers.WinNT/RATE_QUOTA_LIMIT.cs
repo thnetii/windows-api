80d76637121ac74d61d44af631dd93aa031a22f0
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-17 20:15:23 +0100

    RATE_QUOTA_LIMIT

---
+++ src/THNETII.WinApi.Headers.WinNT/RATE_QUOTA_LIMIT.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+using THNETII.InteropServices.Bitwise;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11450
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RATE_QUOTA_LIMIT
+    {
+        private static readonly Bitfield32 bfRatePercent = Bitfield32.DefineLowerBits(7);
+
+        public int RateData;
+        private int dwRatePercent;
+        public int RatePercent
+        {
+            get => bfRatePercent.Read(dwRatePercent);
+            set => bfRatePercent.Write(ref dwRatePercent, value);
+        }
+    }
+}

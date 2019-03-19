ef0db57c4af16af4ba1caea27bd6c25e8e705bf9
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-19 14:10:15 +0100

    Retire SIZE_T

--- src/THNETII.WinApi.Headers.WinNT/QUOTA_LIMITS.cs
+++ src/THNETII.WinApi.Headers.WinNT/QUOTA_LIMITS.cs
@@ -17,27 +17,27 @@ namespace THNETII.WinApi.Native.WinNT
         /// <para>Specifies the amount of paged pool memory assigned to the user. The paged pool is an area of system memory (physical memory used by the operating system) for objects that can be written to disk when they are not being used.</para>
         /// <para>The value set in this member is not enforced by the LSA.</para>
         /// </summary>
-        public SIZE_T PagedPoolLimit;
+        public UIntPtr PagedPoolLimit;
         /// <summary>
         /// <para>Specifies the amount of nonpaged pool memory assigned to the user. The nonpaged pool is an area of system memory for objects that cannot be written to disk but must remain in physical memory as long as they are allocated.</para>
         /// <para>The value set in this member is not enforced by the LSA.</para>
         /// </summary>
-        public SIZE_T NonPagedPoolLimit;
+        public UIntPtr NonPagedPoolLimit;
         /// <summary>
         /// <para>Specifies the minimum set size assigned to the user. The "working set" of a process is the set of memory pages currently visible to the process in physical RAM memory. These pages are present in memory when the application is running and available for an application to use without triggering a page fault.</para>
         /// <para>The value set in this member is not enforced by the LSA.</para>
         /// </summary>
-        public SIZE_T MinimumWorkingSetSize;
+        public UIntPtr MinimumWorkingSetSize;
         /// <summary>
         /// <para>Specifies the maximum set size assigned to the user.</para>
         /// <para>The value set in this member is not enforced by the LSA.</para>
         /// </summary>
-        public SIZE_T MaximumWorkingSetSize;
+        public UIntPtr MaximumWorkingSetSize;
         /// <summary>
         /// <para>Specifies the maximum size, in bytes, of the paging file, which is a reserved space on disk that backs up committed physical memory on the computer.</para>
         /// <para>The value set in this member is not enforced by the LSA.</para>
         /// </summary>
-        public SIZE_T PagefileLimit;
+        public UIntPtr PagefileLimit;
         /// <summary>
         /// <para>Indicates the maximum amount of time the process can run.</para>
         /// <para>The value set in this member is not enforced by the LSA.</para>

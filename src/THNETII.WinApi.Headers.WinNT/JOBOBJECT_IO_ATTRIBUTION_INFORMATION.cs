9a7e180fc78437960c785a1fcb604d33d2f36094
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-13 15:17:29 +0200

    JOBOBJECT_IO_ATTRIBUTION_INFORMATION

---
+++ src/THNETII.WinApi.Headers.WinNT/JOBOBJECT_IO_ATTRIBUTION_INFORMATION.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12005
+    [StructLayout(LayoutKind.Sequential)]
+    public struct JOBOBJECT_IO_ATTRIBUTION_INFORMATION
+    {
+        public JOBOBJECT_IO_ATTRIBUTION_CONTROL_FLAGS ControlFlags;
+
+        public JOBOBJECT_IO_ATTRIBUTION_STATS ReadStats;
+        public JOBOBJECT_IO_ATTRIBUTION_STATS WriteStats;
+    }
+}
---
+++ src/THNETII.WinApi.Headers.WinNT/JOBOBJECT_IO_ATTRIBUTION_STATS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11996
+    [StructLayout(LayoutKind.Sequential)]
+    public struct JOBOBJECT_IO_ATTRIBUTION_STATS
+    {
+        public UIntPtr IoCount;
+        public ulong TotalNonOverlappedQueueTime;
+        public ulong TotalNonOverlappedServiceTime;
+        public ulong TotalSize;
+    }
+}

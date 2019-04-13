480e85fce8efdbc4fa4b55d224f0fc2ffcafd3fd
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-13 12:27:44 +0200

    JOBOBJECT_RATE_CONTROL_TOLERANCE

---
+++ src/THNETII.WinApi.Headers.WinNT/JOBOBJECT_RATE_CONTROL_TOLERANCE.cs
@@ -0,0 +1,10 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11772
+    public enum JOBOBJECT_RATE_CONTROL_TOLERANCE
+    {
+        ToleranceLow = 1,
+        ToleranceMedium,
+        ToleranceHigh
+    }
+}

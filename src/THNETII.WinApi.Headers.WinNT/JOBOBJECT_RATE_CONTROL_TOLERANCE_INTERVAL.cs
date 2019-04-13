43b1f14a74e341b86099b512890cd37ba04c38c4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-13 12:28:41 +0200

    JOBOBJECT_RATE_CONTROL_TOLERANCE_INTERVAL

---
+++ src/THNETII.WinApi.Headers.WinNT/JOBOBJECT_RATE_CONTROL_TOLERANCE_INTERVAL.cs
@@ -0,0 +1,10 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11778
+    public enum JOBOBJECT_RATE_CONTROL_TOLERANCE_INTERVAL
+    {
+        ToleranceIntervalShort = 1,
+        ToleranceIntervalMedium,
+        ToleranceIntervalLong
+    }
+}

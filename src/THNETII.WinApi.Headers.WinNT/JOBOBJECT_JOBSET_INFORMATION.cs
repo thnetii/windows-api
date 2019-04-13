74e880cbbb562ebe254f94595f26cb620da3edda
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-13 12:26:37 +0200

    JOBOBJECT_JOBSET_INFORMATION

---
+++ src/THNETII.WinApi.Headers.WinNT/JOBOBJECT_JOBSET_INFORMATION.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 11768
+    [StructLayout(LayoutKind.Sequential)]
+    public struct JOBOBJECT_JOBSET_INFORMATION
+    {
+        public int MemberLevel;
+    }
+}

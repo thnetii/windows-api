e34168f9bf9f015cd0e7a82ba868f6be51b0693c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-14 13:54:56 +0200

    SYSTEM_PROCESSOR_CYCLE_TIME_INFORMATION

---
+++ src/THNETII.WinApi.Headers.WinNT/SYSTEM_PROCESSOR_CYCLE_TIME_INFORMATION.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12396
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SYSTEM_PROCESSOR_CYCLE_TIME_INFORMATION
+    {
+        public long CycleTime;
+    }
+}

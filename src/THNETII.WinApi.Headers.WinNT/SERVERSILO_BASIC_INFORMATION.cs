c5b90245a65e5477f3a5d99340835a3fed34b170
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-14 11:07:17 +0200

    SERVERSILO_BASIC_INFORMATION

---
+++ src/THNETII.WinApi.Headers.WinNT/SERVERSILO_BASIC_INFORMATION.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12209
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SERVERSILO_BASIC_INFORMATION
+    {
+        public int ServiceSessionId;
+        public SERVERSILO_STATE State;
+        public int ExitStatus;
+    }
+}

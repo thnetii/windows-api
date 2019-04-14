eb537397e8b9036fb188f119e3ed411934ce6300
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-14 14:26:09 +0200

    XSTATE_FEATURE

---
+++ src/THNETII.WinApi.Headers.WinNT/XSTATE_FEATURE.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12557
+    [StructLayout(LayoutKind.Sequential)]
+    public struct XSTATE_FEATURE
+    {
+        public int Offset;
+        public int Size;
+    }
+}

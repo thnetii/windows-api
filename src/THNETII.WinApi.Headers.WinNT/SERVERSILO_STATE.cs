71430aa04b63f5fb84c9d8d2e63f1506391539b6
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-14 11:05:51 +0200

    SERVERSILO_STATE

---
+++ src/THNETII.WinApi.Headers.WinNT/SERVERSILO_STATE.cs
@@ -0,0 +1,12 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12201
+    public enum SERVERSILO_STATE
+    {
+        SERVERSILO_INITING = 0,
+        SERVERSILO_STARTED,
+        SERVERSILO_SHUTTING_DOWN,
+        SERVERSILO_TERMINATING,
+        SERVERSILO_TERMINATED,
+    }
+}

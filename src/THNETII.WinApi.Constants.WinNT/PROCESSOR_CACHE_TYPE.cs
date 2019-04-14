e1f572eca44d51305eac3e280392e582bafe796b
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-14 11:18:53 +0200

    PROCESSOR_CACHE_TYPE

---
+++ src/THNETII.WinApi.Constants.WinNT/PROCESSOR_CACHE_TYPE.cs
@@ -0,0 +1,11 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12266
+    public enum PROCESSOR_CACHE_TYPE
+    {
+        CacheUnified,
+        CacheInstruction,
+        CacheData,
+        CacheTrace
+    }
+}

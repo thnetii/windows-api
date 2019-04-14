f87fbc6a4bc1b03c501da1b9206e832ecc7d03f7
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-14 11:15:52 +0200

    LOGICAL_PROCESSOR_RELATIONSHIP

---
+++ src/THNETII.WinApi.Headers.WinNT/LOGICAL_PROCESSOR_RELATIONSHIP.cs
@@ -0,0 +1,13 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12255
+    public enum LOGICAL_PROCESSOR_RELATIONSHIP
+    {
+        RelationProcessorCore,
+        RelationNumaNode,
+        RelationCache,
+        RelationProcessorPackage,
+        RelationGroup,
+        RelationAll = 0xffff
+    }
+}

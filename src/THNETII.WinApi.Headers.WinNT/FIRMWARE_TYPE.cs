3ae3035e9fecf64387b029245da374645dc14f02
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-14 11:08:14 +0200

    FIRMWARE_TYPE

---
+++ src/THNETII.WinApi.Headers.WinNT/FIRMWARE_TYPE.cs
@@ -0,0 +1,11 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12216
+    public enum FIRMWARE_TYPE
+    {
+        FirmwareTypeUnknown,
+        FirmwareTypeBios,
+        FirmwareTypeUefi,
+        FirmwareTypeMax
+    }
+}

6ee89e3a4c4ddf4fec482233016110c97e6499b0
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-15 12:55:08 +0200

    FILE_SEGMENT_ELEMENT

---
+++ src/THNETII.WinApi.Headers.WinNT/FILE_SEGMENT_ELEMENT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 12990
+    /// <summary>
+    /// Define segement buffer structure for scatter/gather read/write.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct FILE_SEGMENT_ELEMENT
+    {
+        [FieldOffset(0)]
+        public IntPtr Buffer;
+        [FieldOffset(0)]
+        public ulong Alignment;
+    }
+}

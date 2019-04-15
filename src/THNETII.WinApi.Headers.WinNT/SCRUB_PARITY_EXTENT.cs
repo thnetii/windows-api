5694706d193180daeb62c2bc4412f02cf7d5924e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-15 23:49:54 +0200

    SCRUB_PARITY_EXTENT

---
+++ src/THNETII.WinApi.Headers.WinNT/SCRUB_PARITY_EXTENT.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 13268
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SCRUB_PARITY_EXTENT
+    {
+        public long Offset;
+
+        public ulong Length;
+    }
+}

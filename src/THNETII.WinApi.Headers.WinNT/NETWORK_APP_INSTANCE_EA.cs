3405f02738bcd542e31ed54f8a34677ed10d40a0
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-16 08:29:25 +0200

    NETWORK_APP_INSTANCE_EA

---
+++ src/THNETII.WinApi.Headers.WinNT/NETWORK_APP_INSTANCE_EA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 13543
+    [StructLayout(LayoutKind.Sequential)]
+    public struct NETWORK_APP_INSTANCE_EA
+    {
+        /// <summary>
+        /// The caller places a GUID that should always be unique for a single instance of
+        /// the application.
+        /// </summary>
+        public Guid AppInstanceID;
+
+        /// <summary>
+        /// Combination of <see cref="NETWORK_APP_INSTANCE_CSV_FLAGS"/> flags.
+        /// </summary>
+        public NETWORK_APP_INSTANCE_CSV_FLAGS CsvFlags;
+    }
+}

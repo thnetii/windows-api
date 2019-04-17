2fbabefb3fa57ca8ba774c4465a460933303a4c4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 19:31:03 +0200

    POWER_USER_PRESENCE

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_USER_PRESENCE.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15327
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POWER_USER_PRESENCE
+    {
+        public POWER_USER_PRESENCE_TYPE UserPresence;
+    }
+}

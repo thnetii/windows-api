0389d1f85ac1ff8d56b1fc0041d5760097cfe200
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 18:54:26 +0200

    MONITOR_DISPLAY_STATE

---
+++ src/THNETII.WinApi.Headers.WinNT/MONITOR_DISPLAY_STATE.cs
@@ -0,0 +1,10 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15131
+    public enum MONITOR_DISPLAY_STATE
+    {
+        PowerMonitorOff = 0,
+        PowerMonitorOn,
+        PowerMonitorDim
+    }
+}

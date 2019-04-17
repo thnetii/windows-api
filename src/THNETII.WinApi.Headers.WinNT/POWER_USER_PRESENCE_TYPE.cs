245645d1fa75a06ac4681e8b5694e062c5fbba00
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 19:17:41 +0200

    POWER_USER_PRESENCE_TYPE

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_USER_PRESENCE_TYPE.cs
@@ -0,0 +1,13 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15321
+    /// <summary>
+    /// User Presence Values
+    /// </summary>
+    public enum POWER_USER_PRESENCE_TYPE
+    {
+        UserNotPresent = 0,
+        UserPresent = 1,
+        UserUnknown = 0xff
+    }
+}

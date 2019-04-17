febd7dab2ec40c2fd49400662fab42b9ce5642ff
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 18:53:17 +0200

    DEVICE_POWER_STATE

---
+++ src/THNETII.WinApi.Headers.WinNT/DEVICE_POWER_STATE.cs
@@ -0,0 +1,13 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15122
+    public enum DEVICE_POWER_STATE
+    {
+        PowerDeviceUnspecified = 0,
+        PowerDeviceD0,
+        PowerDeviceD1,
+        PowerDeviceD2,
+        PowerDeviceD3,
+        PowerDeviceMaximum
+    }
+}

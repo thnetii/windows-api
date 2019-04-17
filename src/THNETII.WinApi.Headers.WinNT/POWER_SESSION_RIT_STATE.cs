f683429a294a137874eb9b7c99ed3a2f3e50e53c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 19:41:31 +0200

    POWER_SESSION_RIT_STATE

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_SESSION_RIT_STATE.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15344
+    /// <summary>
+    /// Session RIT State
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POWER_SESSION_RIT_STATE
+    {
+        private byte ActiveField;
+        /// <value>
+        /// <see langword="true"/> - RIT input received,
+        /// <see langword="false"/> - RIT timeout
+        /// </value>
+        public bool Active
+        {
+            get => ActiveField != 0;
+            set => ActiveField = (byte)(value ? 1 : 0);
+        }
+        /// <summary>
+        /// last input time held for this session
+        /// </summary>
+        public int LastInputTime;
+    }
+}

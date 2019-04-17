22a46c17c95f930fab844b618959ccd538c6c7eb
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 18:54:47 +0200

    USER_ACTIVITY_PRESENCE

---
+++ src/THNETII.WinApi.Headers.WinNT/USER_ACTIVITY_PRESENCE.cs
@@ -0,0 +1,12 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15137
+    public enum USER_ACTIVITY_PRESENCE
+    {
+        PowerUserPresent = 0,
+        PowerUserNotPresent,
+        PowerUserInactive,
+        PowerUserMaximum,
+        PowerUserInvalid = PowerUserMaximum
+    }
+}

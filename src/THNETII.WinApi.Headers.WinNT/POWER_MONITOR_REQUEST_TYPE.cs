934b21de2a1950ed6635712725e251a2b16a7250
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 19:50:39 +0200

    POWER_MONITOR_REQUEST_TYPE

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_MONITOR_REQUEST_TYPE.cs
@@ -0,0 +1,10 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15425
+    public enum POWER_MONITOR_REQUEST_TYPE
+    {
+        MonitorRequestTypeOff,
+        MonitorRequestTypeOnAndPresent,
+        MonitorRequestTypeToggleOn
+    }
+}

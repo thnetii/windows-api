008baacaf8d2f600ed400120fd7b3064d76ded91
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 19:37:28 +0200

    POWER_SESSION_TIMEOUTS

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_SESSION_TIMEOUTS.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15339
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POWER_SESSION_TIMEOUTS
+    {
+        public int InputTimeout;
+        public int DisplayTimeout;
+    }
+}

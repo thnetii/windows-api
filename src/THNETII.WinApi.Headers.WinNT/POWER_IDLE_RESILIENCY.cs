84040f17b3d9927b0bc2b8d45cb00f27a15c8063
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 19:46:13 +0200

    POWER_IDLE_RESILIENCY

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_IDLE_RESILIENCY.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15361
+    /// <summary>
+    /// Idle resiliency
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POWER_IDLE_RESILIENCY
+    {
+        public int CoalescingTimeout;
+        public int IdleResiliencyPeriod;
+    }
+}

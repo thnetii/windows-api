c8907f811d408d414afe8a6443de99586e24472a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 19:33:51 +0200

    POWER_SESSION_CONNECT

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_SESSION_CONNECT.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15331
+    /// <summary>
+    /// Session Connect/Disconnect
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POWER_SESSION_CONNECT
+    {
+        private byte ConnectedField;
+        public bool Connected
+        {
+            get => ConnectedField != 0;
+            set => ConnectedField = (byte)(value ? 1 : 0);
+        }
+        private byte ConsoleField;
+        public bool Console
+        {
+            get => ConsoleField != 0;
+            set => ConsoleField = (byte)(value ? 1 : 0);
+        }
+    }
+}

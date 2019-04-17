4afc0b370f4ab55a098b082ae20a624a57923ca8
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-17 19:54:35 +0200

    POWER_MONITOR_INVOCATION

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_MONITOR_INVOCATION.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15431
+    /// <summary>
+    /// Monitor invocation
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POWER_MONITOR_INVOCATION
+    {
+        private byte ConsoleField;
+        public bool Console
+        {
+            get => ConsoleField != 0;
+            set => ConsoleField = (byte)(value ? 1 : 0);
+        }
+        public POWER_MONITOR_REQUEST_REASON RequestReason;
+    }
+}

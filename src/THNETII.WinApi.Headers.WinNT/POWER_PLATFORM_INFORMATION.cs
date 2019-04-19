b5f007e9a6c80d5426b7e43032c4dd4dac15e7ef
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-20 00:43:21 +0200

    POWER_PLATFORM_INFORMATION

---
+++ src/THNETII.WinApi.Headers.WinNT/POWER_PLATFORM_INFORMATION.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15559
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POWER_PLATFORM_INFORMATION
+    {
+        private byte AoAcField;
+        public bool AoAc
+        {
+            get => AoAcField != 0;
+            set => AoAcField = (byte)(value ? 1 : 0);
+        }
+    }
+}

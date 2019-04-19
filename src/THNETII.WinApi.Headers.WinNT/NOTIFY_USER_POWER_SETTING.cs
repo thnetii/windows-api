42f993a1a315c5f2e573a2d2a8f14e71682778e5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-20 00:23:08 +0200

    NOTIFY_USER_POWER_SETTING

---
+++ src/THNETII.WinApi.Headers.WinNT/NOTIFY_USER_POWER_SETTING.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 15494
+    [StructLayout(LayoutKind.Sequential)]
+    public struct NOTIFY_USER_POWER_SETTING
+    {
+        public Guid Guid;
+    }
+}

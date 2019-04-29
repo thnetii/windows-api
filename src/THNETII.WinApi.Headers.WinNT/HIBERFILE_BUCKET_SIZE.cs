20802071773137a5602bc342d0bba7c59fce02b6
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-29 12:25:09 +0200

    HIBERFILE_BUCKET_SIZE

---
+++ src/THNETII.WinApi.Headers.WinNT/HIBERFILE_BUCKET_SIZE.cs
@@ -0,0 +1,15 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 16111
+    public enum HIBERFILE_BUCKET_SIZE
+    {
+        HiberFileBucket1GB = 0,
+        HiberFileBucket2GB,
+        HiberFileBucket4GB,
+        HiberFileBucket8GB,
+        HiberFileBucket16GB,
+        HiberFileBucket32GB,
+        HiberFileBucketUnlimited,
+        HiberFileBucketMax
+    }
+}

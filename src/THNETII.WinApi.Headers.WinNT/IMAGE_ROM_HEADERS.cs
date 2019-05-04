e069a01d580bc54dd4c02c862bf83c2c53d4a32f
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-04 14:46:26 +0200

    IMAGE_ROM_HEADERS

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_ROM_HEADERS.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 16548
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct IMAGE_ROM_HEADERS
+    {
+        public IMAGE_FILE_HEADER FileHeader;
+        public IMAGE_ROM_OPTIONAL_HEADER OptionalHeader;
+    }
+}

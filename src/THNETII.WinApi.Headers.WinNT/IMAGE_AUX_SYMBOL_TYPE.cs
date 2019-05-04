57ca4bf967cf693e04f9372057462a8d94ccb829
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-04 18:17:30 +0200

    IMAGE_AUX_SYMBOL_TYPE

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_AUX_SYMBOL_TYPE.cs
@@ -0,0 +1,8 @@
+﻿namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17009
+    public enum IMAGE_AUX_SYMBOL_TYPE
+    {
+        IMAGE_AUX_SYMBOL_TYPE_TOKEN_DEF = 1,
+    }
+}

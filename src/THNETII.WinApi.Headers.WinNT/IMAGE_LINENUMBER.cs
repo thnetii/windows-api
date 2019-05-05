11c5e1e32b38b87338f9b074cd70867917fc803d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-05 14:46:34 +0200

    IMAGE_LINENUMBER

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_LINENUMBER.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 2)]
+    public struct IMAGE_LINENUMBER
+    {
+        public IMAGE_LINENUMBER_TYPE Type;
+        /// <summary>Line number.</summary>
+        public short Linenumber;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Pack = 2)]
+    public struct IMAGE_LINENUMBER_TYPE
+    {
+        /// <summary>Symbol table index of function name if Linenumber is 0.</summary>
+        [FieldOffset(0)]
+        public int SymbolTableIndex;
+        /// <summary>Virtual address of line number.</summary>
+        [FieldOffset(0)]
+        public int VirtualAddress;
+    }
+}

9ef3f9dee2549fc6c3c34ba0eb4141f3e5b51397
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-05 14:46:47 +0200

    IMAGE_BASE_RELOCATION

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_BASE_RELOCATION.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct IMAGE_BASE_RELOCATION
+    {
+        public int VirtualAddress;
+        public int SizeOfBlock;
+        // public short TypeOffset;
+    }
+}

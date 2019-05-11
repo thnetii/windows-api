3956ac179f951a28cd58b239cdf8115dac79b259
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-11 12:38:43 +0200

    IMAGE_BOUND_FORWARDER_REF

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_BOUND_FORWARDER_REF.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17670
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct IMAGE_BOUND_FORWARDER_REF
+    {
+        public int TimeDateStamp;
+        public short OffsetModuleName;
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public short Reserved;
+    }
+}

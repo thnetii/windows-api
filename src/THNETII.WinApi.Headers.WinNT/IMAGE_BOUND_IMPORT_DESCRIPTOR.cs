f40d8e98a3872bb55f2ce1451696c90943a231c0
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-11 12:38:48 +0200

    IMAGE_BOUND_IMPORT_DESCRIPTOR

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_BOUND_IMPORT_DESCRIPTOR.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17663
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct IMAGE_BOUND_IMPORT_DESCRIPTOR
+    {
+        public int TimeDateStamp;
+        public short OffsetModuleName;
+        public short NumberOfModuleForwarderRefs;
+        // Span of zero or more IMAGE_BOUND_FORWARDER_REF follows
+        public unsafe Span<IMAGE_BOUND_FORWARDER_REF> ModuleForwarderRefs
+        {
+            get
+            {
+                fixed (IMAGE_BOUND_IMPORT_DESCRIPTOR* ptr = &this)
+                {
+                    void* pSpan = ptr + 1;
+                    return new Span<IMAGE_BOUND_FORWARDER_REF>(pSpan, NumberOfModuleForwarderRefs);
+                }
+            }
+        }
+    }
+}

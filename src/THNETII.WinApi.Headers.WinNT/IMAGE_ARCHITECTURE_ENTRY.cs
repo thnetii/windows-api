9f5a427d17270a2da61feff940be30c3075fdc85
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-13 00:53:19 +0200

    IMAGE_ARCHITECTURE_HEADER and IMAGE_ARCHITECTURE_ENTRY

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_ARCHITECTURE_ENTRY.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 18430
+    [StructLayout(LayoutKind.Sequential)]
+    public struct IMAGE_ARCHITECTURE_ENTRY
+    {
+        /// <summary>
+        /// RVA of instruction to fixup
+        /// </summary>
+        public int FixupInstRVA;
+        /// <summary>
+        /// fixup instruction (see alphaops.h)
+        /// </summary>
+        public int NewInst;
+    }
+}
---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_ARCHITECTURE_HEADER.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+
+using THNETII.InteropServices.Bitwise;
+using THNETII.WinApi.Helpers;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 18421
+    [StructLayout(LayoutKind.Sequential)]
+    public struct IMAGE_ARCHITECTURE_HEADER
+    {
+        internal int Attributes;
+        private static readonly Bitfield32 AmaskValueBitfield = Bitfield32.Bit(0);
+        private static readonly Bitfield32 MustBeZeroBitfield1 = Bitfield32.SelectBits(1, 7);
+        private static readonly Bitfield32 AmaskShiftBitfield = Bitfield32.SelectBits(8, 8);
+        private static readonly Bitfield32 MustBeZeroBitfield2 = Bitfield32.RemainingBits(16);
+        /// <summary>
+        /// <para><see langword="true"/> -> code section depends on mask bit</para>
+        /// <para><see langword="false"/> -> new instruction depends on mask bit</para>
+        /// </summary>
+        public bool AmaskValue
+        {
+            get => AmaskValueBitfield.ReadBool(Attributes);
+            set => AmaskValueBitfield.WriteBool(ref Attributes, value);
+        }
+        /// <summary>
+        /// Amask bit in question for this fixup
+        /// </summary>
+        public int AmaskShift
+        {
+            get => AmaskShiftBitfield.Read(Attributes);
+            set => AmaskShiftBitfield.Write(ref Attributes, value);
+        }
+        /// <summary>
+        /// RVA into .arch section to array of <see cref="IMAGE_ARCHITECTURE_ENTRY"/>'s
+        /// </summary>
+        public int FirstEntryRVA;
+    }
+}

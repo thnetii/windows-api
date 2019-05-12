7e0399fccac5624518ad4a611d9c6fb7ff14469c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 15:43:15 +0200

    IMAGE_HOT_PATCH_HASHES

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_HOT_PATCH_HASHES.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 18025
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public unsafe struct IMAGE_HOT_PATCH_HASHES
+    {
+        public fixed byte SHA256[32];
+        public fixed byte SHA1[20];
+    }
+}

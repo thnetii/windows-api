4b6cc5e8caa7ebf05514f81218cceb5db930b22f
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 15:27:33 +0200

    IMAGE_PROLOGUE_DYNAMIC_RELOCATION_HEADER

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_PROLOGUE_DYNAMIC_RELOCATION_HEADER.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using THNETII.WinApi.Helpers;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17882
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct IMAGE_PROLOGUE_DYNAMIC_RELOCATION_HEADER
+    {
+        public byte PrologueByteCount;
+        public Span<byte> PrologueBytes =>
+            SpanAfterStruct.GetSpan<IMAGE_PROLOGUE_DYNAMIC_RELOCATION_HEADER, byte>(ref this, PrologueByteCount);
+    }
+}

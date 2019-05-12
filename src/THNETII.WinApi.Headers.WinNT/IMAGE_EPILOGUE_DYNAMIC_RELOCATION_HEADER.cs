b0a5fb9ec86a7b3b96fa182bd910b11a1aab5eea
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 15:28:39 +0200

    IMAGE_EPILOGUE_DYNAMIC_RELOCATION_HEADER

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_EPILOGUE_DYNAMIC_RELOCATION_HEADER.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17888
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct IMAGE_EPILOGUE_DYNAMIC_RELOCATION_HEADER
+    {
+        public int EpilogueCount;
+        public byte EpilogueByteCount;
+        public byte BranchDescriptorElementSize;
+        public short BranchDescriptorCount;
+        // BYTE    BranchDescriptors[...];
+        // BYTE    BranchDescriptorBitMap[...];
+
+    }
+}

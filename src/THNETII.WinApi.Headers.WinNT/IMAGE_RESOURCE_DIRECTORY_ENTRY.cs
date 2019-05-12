41d893e109bdf0aa75948c41d6b4c192ae3d1f8c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 13:06:28 +0200

    IMAGE_RESOURCE_DIRECTORY_ENTRY

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_RESOURCE_DIRECTORY_ENTRY.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+using THNETII.InteropServices.Bitwise;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17728
+    /// <summary>
+    /// Each directory contains the 32-bit Name of the entry and an offset,
+    /// relative to the beginning of the resource directory of the data associated
+    /// with this directory entry.  If the name of the entry is an actual text
+    /// string instead of an integer Id, then the high order bit of the name field
+    /// is set to one and the low order 31-bits are an offset, relative to the
+    /// beginning of the resource directory of the string, which is of type
+    /// <see cref="IMAGE_RESOURCE_DIRECTORY_STRING"/>.  Otherwise the high bit is clear and the
+    /// low-order 16-bits are the integer Id that identify this resource directory
+    /// entry. If the directory entry is yet another resource directory (i.e. a
+    /// subdirectory), then the high order bit of the offset field will be
+    /// set to indicate this.  Otherwise the high bit is clear and the offset
+    /// field points to a resource data entry.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Pack = 4)]
+    public struct IMAGE_RESOURCE_DIRECTORY_ENTRY
+    {
+        private static readonly Bitfield32 NameOffsetBitfield = Bitfield32.LowBits(31);
+        private static readonly Bitfield32 NameIsStringBitfield = Bitfield32.Bit(31);
+        [FieldOffset(0)]
+        public int Name;
+        public int NameOffset
+        {
+            get => NameOffsetBitfield.Read(Name);
+            set => NameOffsetBitfield.Write(ref Name, value);
+        }
+        public bool NameIsString
+        {
+            get => NameIsStringBitfield.ReadMasked(Name) != 0;
+            set => NameIsStringBitfield.WriteMasked(ref Name, value ? ~0 : 0);
+        }
+        [FieldOffset(0)]
+        public short Id;
+        private static readonly Bitfield32 OffsetToDirectoryBitfield = Bitfield32.LowBits(31);
+        private static readonly Bitfield32 DataIsDirectoryBitfield = Bitfield32.Bit(31);
+        [FieldOffset(sizeof(int))]
+        public int OffsetToData;
+        public int OffsetToDirectory
+        {
+            get => OffsetToDirectoryBitfield.Read(OffsetToData);
+            set => OffsetToDirectoryBitfield.Write(ref OffsetToData, value);
+        }
+        public bool DataIsDirectory
+        {
+            get => DataIsDirectoryBitfield.ReadMasked(OffsetToData) != 0;
+            set => DataIsDirectoryBitfield.WriteMasked(ref OffsetToData, value ? ~0 : 0);
+        }
+    }
+}

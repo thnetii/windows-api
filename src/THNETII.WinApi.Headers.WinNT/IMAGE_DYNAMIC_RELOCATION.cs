0160acd0497702f9e874e7ffc69b895657c41571
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 13:34:51 +0200

    IMAGE_DYNAMIC_RELOCATION

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_DYNAMIC_RELOCATION.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17861
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct IMAGE_DYNAMIC_RELOCATION
+    {
+        public IntPtr Symbol;
+        public int BaseRelocSize;
+        public unsafe Span<IMAGE_BASE_RELOCATION> BaseRelocations
+        {
+            get
+            {
+                fixed (IMAGE_DYNAMIC_RELOCATION* ptr = &this)
+                {
+                    void* pSpan = ptr + 1;
+                    return new Span<IMAGE_BASE_RELOCATION>(pSpan, BaseRelocSize);
+                }
+            }
+        }
+    }
+
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17827
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct IMAGE_DYNAMIC_RELOCATION32
+    {
+        public int Symbol;
+        public int BaseRelocSize;
+        public unsafe Span<IMAGE_BASE_RELOCATION> BaseRelocations
+        {
+            get
+            {
+                fixed (IMAGE_DYNAMIC_RELOCATION32* ptr = &this)
+                {
+                    void* pSpan = ptr + 1;
+                    return new Span<IMAGE_BASE_RELOCATION>(pSpan, BaseRelocSize);
+                }
+            }
+        }
+    }
+
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17833
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct IMAGE_DYNAMIC_RELOCATION64
+    {
+        public long Symbol;
+        public int BaseRelocSize;
+        public unsafe Span<IMAGE_BASE_RELOCATION> BaseRelocations
+        {
+            get
+            {
+                fixed (IMAGE_DYNAMIC_RELOCATION64* ptr = &this)
+                {
+                    void* pSpan = ptr + 1;
+                    return new Span<IMAGE_BASE_RELOCATION>(pSpan, BaseRelocSize);
+                }
+            }
+        }
+    }
+}

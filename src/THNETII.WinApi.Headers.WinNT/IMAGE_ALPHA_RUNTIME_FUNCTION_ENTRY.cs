d2c8d10f7ee32942a38eb13e0a9ad7576dc251cc
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 22:48:42 +0200

    IMAGE_ALPHA_RUNTIME_FUNCTION_ENTRY

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_ALPHA_RUNTIME_FUNCTION_ENTRY.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 18131
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct IMAGE_ALPHA_RUNTIME_FUNCTION_ENTRY
+    {
+        public int BeginAddress;
+        public int EndAddress;
+        public int ExceptionHandler;
+        public int HandlerData;
+        public int PrologEndAddress;
+    }
+}

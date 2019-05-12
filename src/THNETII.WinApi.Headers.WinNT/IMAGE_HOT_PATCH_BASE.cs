035a58ac4303eba7f235a780ffa7161391aec4b6
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 15:43:10 +0200

    IMAGE_HOT_PATCH_BASE

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_HOT_PATCH_BASE.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 18014
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct IMAGE_HOT_PATCH_BASE
+    {
+        public int SequenceNumber;
+        public int Flags;
+        public int OriginalTimeDateStamp;
+        public int OriginalCheckSum;
+        public int CodeIntegrityInfo;
+        public int CodeIntegritySize;
+        public int PatchTable;
+        public int BufferOffset; // V2 and later
+    }
+}

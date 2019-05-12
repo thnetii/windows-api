462d625b888da2c5e2a9e67142bf5ad6382097a3
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 15:49:31 +0200

    IMAGE_GUARD_* constants

--- src/THNETII.WinApi.Constants.WinNT/WinNTConstants.cs
+++ src/THNETII.WinApi.Constants.WinNT/WinNTConstants.cs
@@ -5769,6 +5769,39 @@ namespace THNETII.WinApi.Native.WinNT
         public const int IMAGE_HOT_PATCH_NO_CALL_TARGET = 0x00064000;
         public const int IMAGE_HOT_PATCH_DYNAMIC_VALUE = 0x00078000;
 
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 18049
+        /// <summary>Module performs control flow integrity checks using system-supplied support</summary>
+        public const int IMAGE_GUARD_CF_INSTRUMENTED = 0x00000100;
+        /// <summary>Module performs control flow and write integrity checks</summary>
+        public const int IMAGE_GUARD_CFW_INSTRUMENTED = 0x00000200;
+        /// <summary>Module contains valid control flow target metadata</summary>
+        public const int IMAGE_GUARD_CF_FUNCTION_TABLE_PRESENT = 0x00000400;
+        /// <summary>Module does not make use of the /GS security cookie</summary>
+        public const int IMAGE_GUARD_SECURITY_COOKIE_UNUSED = 0x00000800;
+        /// <summary>Module supports read only delay load IAT</summary>
+        public const int IMAGE_GUARD_PROTECT_DELAYLOAD_IAT = 0x00001000;
+        /// <summary>Delayload import table in its own .didat section (with nothing else in it) that can be freely reprotected</summary>
+        public const int IMAGE_GUARD_DELAYLOAD_IAT_IN_ITS_OWN_SECTION = 0x00002000;
+        /// <summary>
+        /// Module contains suppressed export information. This also infers that the address
+        /// taken IAT table is also present in the load config.
+        /// </summary>
+        public const int IMAGE_GUARD_CF_EXPORT_SUPPRESSION_INFO_PRESENT = 0x00004000;
+        /// <summary>Module enables suppression of exports</summary>
+        public const int IMAGE_GUARD_CF_ENABLE_EXPORT_SUPPRESSION = 0x00008000;
+        /// <summary>Module contains longjmp target information</summary>
+        public const int IMAGE_GUARD_CF_LONGJUMP_TABLE_PRESENT = 0x00010000;
+        /// <summary>Module contains return flow instrumentation and metadata</summary>
+        public const int IMAGE_GUARD_RF_INSTRUMENTED = 0x00020000;
+        /// <summary>Module requests that the OS enable return flow protection</summary>
+        public const int IMAGE_GUARD_RF_ENABLE = 0x00040000;
+        /// <summary>Module requests that the OS enable return flow protection in strict mode</summary>
+        public const int IMAGE_GUARD_RF_STRICT = 0x00080000;
+        /// <summary>Stride of Guard CF function table encoded in these bits (additional count of bytes per element)</summary>
+        public const int IMAGE_GUARD_CF_FUNCTION_TABLE_SIZE_MASK = unchecked((int)0xF0000000);
+        /// <summary>Shift to right-justify Guard CF function table stride</summary>
+        public const int IMAGE_GUARD_CF_FUNCTION_TABLE_SIZE_SHIFT = 28;
+
         // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 19775
         public const int RTL_UMS_VERSION = 0x0100;
     }

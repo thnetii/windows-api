00b8a97ff85fffe3fb390a3e9b7f8f6370053ae7
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 13:21:30 +0200

    IMAGE_LOAD_CONFIG_CODE_INTEGRITY

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_LOAD_CONFIG_CODE_INTEGRITY.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17804
+    /// <summary>
+    /// Code Integrity in loadconfig (CI)
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct IMAGE_LOAD_CONFIG_CODE_INTEGRITY
+    {
+        /// <summary>Flags to indicate if CI information is available, etc.</summary>
+        public short Flags;
+        /// <summary><c>0xFFFF</c> means not available</summary>
+        public short Catalog;
+        public int CatalogOffset;
+        /// <summary>Additional bitmask to be defined later</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public int Reserved;
+    }
+}

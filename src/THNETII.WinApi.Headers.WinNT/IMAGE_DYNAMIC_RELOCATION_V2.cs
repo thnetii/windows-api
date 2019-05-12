a7b71d6cfa8561bb0cdf549ab2944772cfcc853c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 15:22:51 +0200

    IMAGE_DYNAMIC_RELOCATION_V2

--- src/THNETII.WinApi.Headers.WinNT/IMAGE_DYNAMIC_RELOCATION_V2.cs
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_DYNAMIC_RELOCATION_V2.cs
@@ -17,4 +17,32 @@ namespace THNETII.WinApi.Native.WinNT
         // BYTE    FixupInfo[FixupInfoSize]
         public Span<byte> FixupInfo => SpanAfterStruct.GetSpan<IMAGE_DYNAMIC_RELOCATION_V2, byte>(ref this, FixupInfoSize);
     }
+
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17861
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct IMAGE_DYNAMIC_RELOCATION32_V2
+    {
+        public int HeaderSize;
+        public int FixupInfoSize;
+        public int Symbol;
+        public int SymbolGroup;
+        public int Flags;
+        // ...     variable length header fields
+        // BYTE    FixupInfo[FixupInfoSize]
+        public Span<byte> FixupInfo => SpanAfterStruct.GetSpan<IMAGE_DYNAMIC_RELOCATION32_V2, byte>(ref this, FixupInfoSize);
+    }
+
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17861
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct IMAGE_DYNAMIC_RELOCATION64_V2
+    {
+        public int HeaderSize;
+        public int FixupInfoSize;
+        public long Symbol;
+        public int SymbolGroup;
+        public int Flags;
+        // ...     variable length header fields
+        // BYTE    FixupInfo[FixupInfoSize]
+        public Span<byte> FixupInfo => SpanAfterStruct.GetSpan<IMAGE_DYNAMIC_RELOCATION64_V2, byte>(ref this, FixupInfoSize);
+    }
 }

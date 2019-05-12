913600b29998760db5f83ad0f91106e097738634
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-12 13:06:34 +0200

    IMAGE_RESOURCE_DIRECTORY

---
+++ src/THNETII.WinApi.Headers.WinNT/IMAGE_RESOURCE_DIRECTORY.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinNT
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winnt.h, line 17716
+    /// <summary>
+    /// Resource directory consists of two counts, following by a variable length
+    /// array of directory entries.  The first count is the number of entries at
+    /// beginning of the array that have actual names associated with each entry.
+    /// The entries are in ascending order, case insensitive strings.  The second
+    /// count is the number of entries that immediately follow the named entries.
+    /// This second count identifies the number of entries that have 16-bit integer
+    /// Ids as their name.  These entries are also sorted in ascending order.
+    /// <para>
+    /// This structure allows fast lookup by either name or number, but for any
+    /// given resource entry only one form of lookup is supported, not both.
+    /// This is consistant with the syntax of the .RC file and the .RES file.
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct IMAGE_RESOURCE_DIRECTORY
+    {
+        public int Characteristics;
+        public int TimeDateStamp;
+        public short MajorVersion;
+        public short MinorVersion;
+        public Version Version
+        {
+            get => new Version(MajorVersion, MinorVersion);
+            set
+            {
+                MajorVersion = (short)(value?.Major ?? 0);
+                MinorVersion = (short)(value?.Minor ?? 0);
+            }
+        }
+        public short NumberOfNamedEntries;
+        public short NumberOfIdEntries;
+        public unsafe Span<IMAGE_RESOURCE_DIRECTORY_ENTRY> DirectoryEntries
+        {
+            get
+            {
+                fixed (IMAGE_RESOURCE_DIRECTORY* ptr = &this)
+                {
+                    void* pSpan = ptr + 1;
+                    return new Span<IMAGE_RESOURCE_DIRECTORY_ENTRY>(pSpan, NumberOfNamedEntries + NumberOfIdEntries);
+                }
+            }
+        }
+    }
+}

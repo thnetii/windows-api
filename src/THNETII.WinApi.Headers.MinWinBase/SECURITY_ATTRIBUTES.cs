122328d496b198e88d10eda3f384b6e2298a9de4
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2019-10-09 16:00:04 +0200

    SECURITY_ATTRIBUTES

---
+++ src/THNETII.WinApi.Headers.MinWinBase/SECURITY_ATTRIBUTES.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.MinWinBase
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\minwinbase.h, line 46
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SECURITY_ATTRIBUTES
+    {
+        public int nLength;
+        public IntPtr lpSecurityDescriptor;
+        [MarshalAs(UnmanagedType.Bool)]
+        internal int dwInheritHandle;
+
+        public bool bInheritHandle
+        {
+            get => dwInheritHandle != 0;
+            set => dwInheritHandle = value ? 1 : 0;
+        }
+    }
+}

721f60ee9deb5f059c272c41d62e1e8fa391bb87
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2019-10-10 11:36:40 +0200

    CREATE_THREAD_DEBUG_INFO

--- src-native-c/THNETII.WinApi.Sample.Native/main.c
+++ src-native-c/THNETII.WinApi.Sample.Native/main.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 {
     WIN32_FIND_DATAA instance;
     const int size = sizeof(instance);
-    const int value = POWER_REQUEST_CONTEXT_DETAILED_STRING;
+    const int value = THREAD_GET_CONTEXT;
 
     const void* ptr = RtlCaptureStackBackTrace;
 
---
+++ src/THNETII.WinApi.Headers.MinWinBase/CREATE_THREAD_DEBUG_INFO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using THNETII.WinApi.Native.WinNT;
+
+namespace THNETII.WinApi.Native.MinWinBase
+{
+    using static WinNTConstants;
+
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\minwinbase.h, line 305
+    /// <summary>
+    /// Contains thread-creation information that can be used by a debugger.
+    /// </summary>
+    /// <remarks>
+    /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-create_thread_debug_info">CREATE_THREAD_DEBUG_INFO structure</a></para>
+    /// </remarks>
+    /// <seealso cref="CREATE_PROCESS_DEBUG_INFO"/>
+    /// <seealso cref="DEBUG_EVENT"/>
+    /// <seealso href="https://docs.microsoft.com/windows/desktop/Debug/debugging-structures">Debugging Structures</seealso>
+    /// <seealso cref="LOAD_DLL_DEBUG_INFO"/>
+    /// <seealso cref="SetThreadContext"/>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CREATE_THREAD_DEBUG_INFO
+    {
+        /// <summary>
+        /// A handle to the thread whose creation caused the debugging event. If this member is <see cref="IntPtr.Zero"/>, the handle is not valid. Otherwise, the debugger has <see cref="THREAD_GET_CONTEXT"/>, <see cref="THREAD_SET_CONTEXT"/>, and <see cref="THREAD_SUSPEND_RESUME"/> access to the thread, allowing the debugger to read from and write to the registers of the thread and control execution of the thread.
+        /// </summary>
+        public IntPtr hThread;
+        /// <summary>
+        /// A pointer to a block of data. At offset <c>0x2C</c> into this block is another pointer, called <c>ThreadLocalStoragePointer</c>, that points to an array of per-module thread local storage blocks. This gives a debugger access to per-thread data in the threads of the process being debugged using the same algorithms that a compiler would use.
+        /// </summary>
+        public IntPtr lpThreadLocalBase;
+        /// <summary>
+        /// A pointer to the starting address of the thread. This value may only be an approximation of the thread's starting address, because any application with appropriate access to the thread can change the thread's context by using the <see cref="SetThreadContext"/> function.
+        /// </summary>
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public LPTHREAD_START_ROUTINE lpStartAddress;
+    }
+}

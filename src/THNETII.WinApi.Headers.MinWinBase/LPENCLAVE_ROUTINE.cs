555bcc4ac57049ea8dbc5a38efb44f2686686c0c
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2019-10-10 11:25:20 +0200

    LPTHREAD_START_ROUTINE, LPENCLAVE_ROUTINE callback functions

---
+++ src/THNETII.WinApi.Headers.MinWinBase/LPENCLAVE_ROUTINE.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.MinWinBase
+{
+    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+    public delegate int LPENCLAVE_ROUTINE(IntPtr lpThreadParameter);
+}
---
+++ src/THNETII.WinApi.Headers.MinWinBase/LPTHREAD_START_ROUTINE.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.MinWinBase
+{
+    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+    public delegate int LPTHREAD_START_ROUTINE(IntPtr lpThreadParameter);
+}

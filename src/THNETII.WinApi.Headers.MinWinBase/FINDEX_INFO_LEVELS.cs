3ce52c854d3a2c7fd42017513a3714cf09be2852
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2019-10-10 09:37:54 +0200

    FINDEX_INFO_LEVELS

---
+++ src/THNETII.WinApi.Headers.MinWinBase/FINDEX_INFO_LEVELS.cs
@@ -0,0 +1,27 @@
+﻿namespace THNETII.WinApi.Native.MinWinBase
+{
+    /// <summary>
+    /// Defines values that are used with the <see cref="FindFirstFileEx"/> function to specify the information level of the returned data.
+    /// </summary>
+    /// <remarks>
+    /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/win32/api/minwinbase/ne-minwinbase-win32_find_dataw">FINDEX_INFO_LEVELS Enumeration</a></para>
+    /// </remarks>
+    /// <seealso cref="FindFirstFileEx"/>
+    /// <seealso cref="WIN32_FIND_DATAW"/>
+    public enum FINDEX_INFO_LEVELS
+    {
+        /// <summary>
+        /// The <see cref="FindFirstFileEx"/> function retrieves a standard set of attribute information. The data is returned in a <see cref="WIN32_FIND_DATAW"/> structure.
+        /// </summary>
+        FindExInfoStandard,
+        /// <summary>
+        /// The <see cref="FindFirstFileEx"/> function does not query the short file name, improving overall enumeration speed. The data is returned in a <see cref="WIN32_FIND_DATAW"/> structure, and the <see cref="WIN32_FIND_DATAW.cAlternateFileName"/> member is always an empty string.
+        /// <para><strong>Windows Server 2008, Windows Vista, Windows Server 2003 and Windows XP:</strong> This value is not supported until Windows Server 2008 R2 and Windows 7.</para>
+        /// </summary>
+        FindExInfoBasic,
+        /// <summary>
+        /// This value is used for validation. Supported values are less than this value.
+        /// </summary>
+        FindExInfoMaxInfoLevel
+    }
+}

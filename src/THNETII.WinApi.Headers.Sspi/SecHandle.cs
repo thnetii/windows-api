746654f945b792b0cb28c1217a60f2c4607fe51f
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-21 12:41:57 +0100

    SecHandle structure, sspi.h: line 134

---
+++ src/THNETII.WinApi.Headers.Sspi/SecHandle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 129
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SecHandle
+    {
+        public UIntPtr dwLower;
+        public UIntPtr dwUpper;
+    }
+}

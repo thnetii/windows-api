132af70cbfc87b9504ba4206167b167fb26ed7ba
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-22 20:49:09 +0100

    SecPkgContext_SessionKey structure, sspi.h line 946

---
+++ src/THNETII.WinApi.Headers.Sspi/SecPkgContext_LogoffTime.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 940
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SecPkgContext_LogoffTime
+    {
+        public TimeStamp tsLogoffTime;
+    }
+}
---
+++ src/THNETII.WinApi.Headers.Sspi/SecPkgContext_SessionKey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 946
+    /// <summary>
+    /// The <see cref="SecPkgContext_SessionKey"/> structure contains information about the session key used for the <a href="https://docs.microsoft.com/windows/desktop/SecGloss/s-gly">security context</a>. This structure is returned by the <see cref="QueryContextAttributes"/> (General) function.
+    /// </summary>
+    /// <remarks>
+    /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/win32/api/sspi/ns-sspi-secpkgcontext_sessionkey">SecPkgContext_SessionKey structure</a></para>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SecPkgContext_SessionKey
+    {
+        /// <summary>
+        /// Size, in bytes, of the session key.
+        /// </summary>
+        public int SessionKeyLength;
+        public byte* SessionKeyPointer;
+        /// <summary>
+        /// The session key for the security context.
+        /// </summary>
+        public Span<byte> SessionKey => new Span<byte>(SessionKeyPointer, SessionKeyLength);
+    }
+}

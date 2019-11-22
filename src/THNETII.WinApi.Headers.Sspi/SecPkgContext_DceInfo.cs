e3c2e08120532e976d7c652027dc336474ade755
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-22 18:20:52 +0100

    SecPkgContext_DceInfo structure, sspi.h line 850

---
+++ src/THNETII.WinApi.Headers.Sspi/SecPkgContext_DceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 846
+    /// <summary>
+    /// The <see cref="SecPkgContext_DceInfo"/> structure contains authorization data used by DCE services. The <see cref="QueryContextAttributes"/> (General) function uses this structure.
+    /// </summary>
+    /// <remarks>
+    /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/win32/api/sspi/ns-sspi-secpkgcontext_dceinfo">SecPkgContext_DceInfo structure</a></para>
+    /// </remarks>
+    /// <seealso cref="QueryContextAttributes"/>
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SecPkgContext_DceInfo
+    {
+        public int AuthzSvc;
+        public void* pPac;
+    }
+}

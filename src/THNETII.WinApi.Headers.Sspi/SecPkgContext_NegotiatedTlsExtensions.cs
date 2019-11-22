e5d63ca7a36e1988cb6f592e2f2765b2ee51ce9c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-23 00:23:25 +0100

    SecPkgContext_NegotiatedTlsExtensions structure, sspi.h line 1135

---
+++ src/THNETII.WinApi.Headers.Sspi/SecPkgContext_NegotiatedTlsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 1123
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SecPkgContext_NegotiatedTlsExtensions
+    {
+        /// <summary>
+        /// Number of negotiated TLS extensions.
+        /// </summary>
+        public int ExtensionsCount;
+        /// <summary>
+        /// Pointer to array of 2-byte TLS extension IDs (allocated by IANA).
+        /// </summary>
+        public ushort* ExtensionsPointer;
+        /// <summary>
+        /// Span of 2-byte TLS extension IDs (allocated by IANA).
+        /// </summary>
+        public Span<ushort> Extensions =>
+            new Span<ushort>(ExtensionsPointer, ExtensionsCount);
+    }
+}

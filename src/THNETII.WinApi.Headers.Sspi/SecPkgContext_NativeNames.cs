f31da1dffb409d1103d0506043f8ee8e5aa11674
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-22 21:32:37 +0100

    SecPkgContext_NativeNames structure, sspi.h line 1047

---
+++ src/THNETII.WinApi.Headers.Sspi/SecPkgContext_NativeNames.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 1028
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SecPkgContext_NativeNamesW
+    {
+        public LPWSTR sClientName;
+        public LPWSTR sServerName;
+    }
+
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 1034
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SecPkgContext_NativeNamesA
+    {
+        public LPSTR sClientName;
+        public LPSTR sServerName;
+    }
+}

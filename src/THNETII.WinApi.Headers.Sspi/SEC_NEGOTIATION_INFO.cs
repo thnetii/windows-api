512e496def9b93e91d2f015191d6ab0765081e42
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-22 09:51:55 +0100

    SEC_NEGOTIATION_INFO structure, sspi.h: line 381

---
+++ src/THNETII.WinApi.Headers.Sspi/SEC_NEGOTIATION_INFO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 376
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SEC_NEGOTIATION_INFO
+    {
+        /// <summary>Size of this structure</summary>
+        public int Size;
+        /// <summary>Length of name hint</summary>
+        public int NameLength;
+        #region public Span<char> Name;
+        internal char* NamePtr;
+        /// <summary>Name hint</summary>
+        public Span<char> Name => new Span<char>(NamePtr, NameLength);
+        #endregion
+        #region public void* Reserved;
+        /// <summary>Reserved</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public void* Reserved;
+        #endregion
+    }
+}

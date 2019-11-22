f0a2510e141647d90c916cde193074817c0e71f6
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-23 00:01:25 +0100

    SecPkgContext_Target structure, sspi.h line 1100

---
+++ src/THNETII.WinApi.Headers.Sspi/SecPkgContext_Target.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 1100
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SecPkgContext_Target
+    {
+        public int TargetLength;
+        public byte* TargetPointer;
+        public Span<byte> Target => new Span<byte>(TargetPointer, TargetLength);
+    }
+}

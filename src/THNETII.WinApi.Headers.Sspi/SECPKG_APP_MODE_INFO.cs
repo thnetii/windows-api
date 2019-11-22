ace653d431965f6c50adea58cd8610cd5454fa51
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-23 00:27:44 +0100

    SECPKG_APP_MODE_INFO structure, sspi.h line 1143

---
+++ src/THNETII.WinApi.Headers.Sspi/SECPKG_APP_MODE_INFO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 1137
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SECPKG_APP_MODE_INFO
+    {
+        public int UserFunction;
+        public IntPtr Argument1;
+        public IntPtr Argument2;
+        public SecBuffer UserData;
+        #region public bool ReturnToLsa;
+        internal byte ReturnToLsaField;
+        public bool ReturnToLsa
+        {
+            get => ReturnToLsaField != 0;
+            set => ReturnToLsaField = (byte)(value ? 1 : 0);
+        }
+        #endregion
+    }
+}

e4ab83dc7d4aa6f0b5cc2768eb03c3c87d48f84c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-22 10:33:56 +0100

    SEC_TOKEN_BINDING, sspi.h: line 425

---
+++ src/THNETII.WinApi.Headers.Sspi/SEC_TOKEN_BINDING.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 425
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SEC_TOKEN_BINDING
+    {
+        /// <summary>Supported major version of the Token Binding protocol</summary>
+        public byte MajorVersion;
+        /// <summary>Supported minor version of the Token Binding protocol</summary>
+        public byte MinorVersion;
+        /// <summary>Size in bytes of the Token Binding key parameter IDs array</summary>
+        public ushort KeyParametersSize;
+        #region public Span<byte> KeyParameters;
+        internal byte* KeyParametersPtr;
+        /// <summary>Token Binding key parameter IDs, most preferred first</summary>
+        public Span<byte> KeyParameters => new Span<byte>(KeyParametersPtr, KeyParametersSize);
+        #endregion
+    }
+}

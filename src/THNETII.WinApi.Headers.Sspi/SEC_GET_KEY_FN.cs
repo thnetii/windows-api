8770e33ab501fd748be6b38a07851e8704d2341e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-23 00:33:54 +0100

    SEC_GET_KEY_FN callback function, sspi.h 1148

---
+++ src/THNETII.WinApi.Headers.Sspi/SEC_GET_KEY_FN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    /// <summary/>
+    /// <param name="Arg">Argument passed in</param>
+    /// <param name="Principal">Principal ID</param>
+    /// <param name="KeyVer">Key Version</param>
+    /// <param name="Key">Returned ptr to key</param>
+    /// <param name="Status">returned status</param>
+    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+    public delegate void SEC_GET_KEY_FN(
+        IntPtr Arg,
+        IntPtr Principal,
+        int KeyVer,
+        out IntPtr Key,
+        out int Status
+        );
+}

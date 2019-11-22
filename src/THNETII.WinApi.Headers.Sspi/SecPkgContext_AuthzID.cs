44065aea532d5addfe58323820281b042b82adb4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-23 00:01:02 +0100

    SecPkgContext_AuthzID structure, sspi.h line 1093

---
+++ src/THNETII.WinApi.Headers.Sspi/SecPkgContext_AuthzID.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.Sspi
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\sspi.h, line 1088
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct SecPkgContext_AuthzID
+    {
+        public int AuthzIDLength;
+        public byte* AuthzIDPointer;
+        public Span<byte> AuthzID => new Span<byte>(AuthzIDPointer, AuthzIDLength);
+    }
+}

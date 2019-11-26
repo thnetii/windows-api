5c873d704c3886ee5c6ba4587ef0bc041e2a54b5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-26 03:32:54 +0100

    SecPkgCred_CipherStrengths structure, minschannel.h line 90

---
+++ src/THNETII.WinApi.Headers.MinSchannel/SecPkgCred_CipherStrengths.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.MinSchannel
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\minschannel.h, line 85
+    /// <summary>
+    /// The <see cref="SecPkgCred_CipherStrengths"/> structure holds the minimum and maximum strength permitted for the cipher used by the specified Schannel credential. This structure is used by the <see cref="QueryCredentialsAttributes"/> function.
+    /// </summary>
+    /// <remarks>
+    /// For information about setting the cipher strength, see <see cref="AcquireCredentialsHandle"/>.
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SecPkgCred_CipherStrengths
+    {
+        /// <summary>
+        /// Minimum cipher strength allowed.
+        /// </summary>
+        public int dwMinimumCipherStrength;
+        /// <summary>
+        /// Maximum cipher strength allowed.
+        /// </summary>
+        public int dwMaximumCipherStrength;
+    }
+}

c6ca7acfeca73c35a00387af5efdf07bbd68c758
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-11-28 17:46:25 +0100

    WinSmcrd Constants, winsmcrd.h

---
+++ src/THNETII.WinApi.Headers.WinSmcrd/WinSmcrdFunctions.cs
@@ -0,0 +1,11 @@
+﻿namespace THNETII.WinApi.Native.WinSmcrd
+{
+    public static class WinSmcrdFunctions
+    {
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\winsmcrd.h, line 138
+        #region SCARD_ATTR_VALUE macro
+        public static int SCARD_ATTR_VALUE(int Class, int Tag) =>
+            (Class << 16) | Tag;
+        #endregion
+    }
+}

ca91768824e29705cb8c8a948ef93e0544476270
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-12-01 13:44:23 +0100

    SCARD_T1_REQUEST structure, winsmcrd.h line 315

---
+++ src/THNETII.WinApi.Headers.WinSmcrd/SCARD_T1_REQUEST.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace THNETII.WinApi.Native.WinSmcrd
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\winsmcrd.h, line 312
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SCARD_T1_REQUEST
+    {
+        public SCARD_IO_REQUEST ioRequest;
+    }
+}

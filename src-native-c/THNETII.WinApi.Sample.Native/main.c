654b7e99b8ec7a1240306cd5ca74b1766956d56d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-12-01 14:03:59 +0100

    SCardErr constants, scarderr.h

--- src-native-c/THNETII.WinApi.Sample.Native/main.c
+++ src-native-c/THNETII.WinApi.Sample.Native/main.c
@@ -1,13 +1,13 @@
 #include <Windows.h>
-#include <WinBase.h>
+#include <winscard.h>
 
 int main(int argc, char* argv[])
 {
-    OSVERSIONINFOW instance;
+    DWORD instance;
     const int size = sizeof(instance);
-    const int value = VER_PLATFORM_WIN32_NT;
+    const int value = SCARD_S_SUCCESS;
 
-    const void* ptr = GetVersionEx;
+    const void* ptr = NULL;
 
     return EXIT_SUCCESS;
 }

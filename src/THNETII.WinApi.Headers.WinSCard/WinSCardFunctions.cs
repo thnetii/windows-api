6873eb010218855a642f7635ec8ce3dbb96cf66e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-01-26 03:51:42 +0100

    SCardGetStatusChange functions, winscard.h line 662

--- src/THNETII.WinApi.Headers.WinSCard/WinSCardFunctions.cs
+++ src/THNETII.WinApi.Headers.WinSCard/WinSCardFunctions.cs
@@ -2646,7 +2646,7 @@ namespace THNETII.WinApi.Native.WinSCard
                     );
         }
         [DllImport(WinSCard, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Ansi)]
-        public static extern unsafe int SCardLocateCardsByATRA(
+        private static extern unsafe int SCardLocateCardsByATRA(
             [In] SCARDCONTEXT hContext,
             [In] SCARD_ATRMASK* rgAtrMasks,
             [In] int cAtrs,
@@ -2674,7 +2674,7 @@ namespace THNETII.WinApi.Native.WinSCard
                     );
         }
         [DllImport(WinSCard, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)]
-        public static extern unsafe int SCardLocateCardsByATRW(
+        private static extern unsafe int SCardLocateCardsByATRW(
             [In] SCARDCONTEXT hContext,
             [In] SCARD_ATRMASK* rgAtrMasks,
             [In] int cAtrs,
@@ -2725,11 +2725,123 @@ namespace THNETII.WinApi.Native.WinSCard
                     rgReaderStates.Length
                     );
         }
-        [DllImport(WinSCard, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)]
-        public static extern unsafe int SCardLocateCardsByATR(
+        [DllImport(WinSCard, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        private static extern unsafe int SCardLocateCardsByATR(
             [In] SCARDCONTEXT hContext,
             [In] SCARD_ATRMASK* rgAtrMasks,
             [In] int cAtrs,
+            [In, Out] SCARD_READERSTATE* rgReaderStates,
+            [In] int cReaders
+            );
+#endif // !NETSTANDARD1_3
+        #endregion
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winscard.h, line 647
+        #region SCardGetStatusChangeA function
+        /// <inheritdoc cref="SCardGetStatusChange(SCARDCONTEXT, TimeSpan, Span{SCARD_READERSTATE})"/>
+        public static unsafe int SCardGetStatusChangeA(
+            SCARDCONTEXT hContext,
+            TimeSpan dwTimeout,
+            Span<SCARD_READERSTATEA> rgReaderStates
+            )
+        {
+            fixed (SCARD_READERSTATEA* ptrReaderStates = rgReaderStates)
+                return SCardGetStatusChangeA(
+                    hContext,
+                    (int)dwTimeout.TotalMilliseconds,
+                    ptrReaderStates,
+                    rgReaderStates.Length
+                    );
+        }
+        [DllImport(WinSCard, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Ansi)]
+        private static extern unsafe int SCardGetStatusChangeA(
+            [In] SCARDCONTEXT hContext,
+            [In] int dwTimeout,
+            [In, Out] SCARD_READERSTATEA* rgReaderStates,
+            [In] int cReaders
+            );
+        #endregion
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winscard.h, line 653
+        #region SCardGetStatusChangeW function
+        /// <inheritdoc cref="SCardGetStatusChange(SCARDCONTEXT, TimeSpan, Span{SCARD_READERSTATE})"/>
+        public static unsafe int SCardGetStatusChangeW(
+            SCARDCONTEXT hContext,
+            TimeSpan dwTimeout,
+            Span<SCARD_READERSTATEW> rgReaderStates
+            )
+        {
+            fixed (SCARD_READERSTATEW* ptrReaderStates = rgReaderStates)
+                return SCardGetStatusChangeW(
+                    hContext,
+                    (int)dwTimeout.TotalMilliseconds,
+                    ptrReaderStates,
+                    rgReaderStates.Length
+                    );
+        }
+        [DllImport(WinSCard, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)]
+        private static extern unsafe int SCardGetStatusChangeW(
+            [In] SCARDCONTEXT hContext,
+            [In] int dwTimeout,
+            [In, Out] SCARD_READERSTATEW* rgReaderStates,
+            [In] int cReaders
+            );
+        #endregion
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\winscard.h, line 659
+        #region SCardGetStatusChange function
+#if !NETSTANDARD1_3
+        /// <summary>
+        /// The <see cref="SCardGetStatusChange(SCARDCONTEXT, TimeSpan, Span{SCARD_READERSTATE})"/> function blocks execution until the current availability of the cards in a specific set of readers changes.
+        /// <para>The caller supplies a list of <a href="https://docs.microsoft.com/windows/desktop/SecGloss/r-gly">readers</a> to be monitored by an <see cref="SCARD_READERSTATE"/> array and the maximum amount of time (in milliseconds) that it is willing to wait for an action to occur on one of the listed readers. Note that <see cref="SCardGetStatusChange(SCARDCONTEXT, TimeSpan, Span{SCARD_READERSTATE})"/> uses the user-supplied value in the <see cref="SCARD_READERSTATE.dwCurrentState"/> members of the <paramref name="rgReaderStates"/> <see cref="SCARD_READERSTATE"/> array as the definition of the current <a href="https://docs.microsoft.com/windows/desktop/SecGloss/s-gly">state</a> of the readers. The function returns when there is a change in availability, having filled in the <see cref="SCARD_READERSTATE.dwEventState"/> members of <paramref name="rgReaderStates"/> appropriately.</para>
+        /// </summary>
+        /// <param name="hContext">A handle that identifies the <a href="https://docs.microsoft.com/windows/desktop/SecGloss/r-gly">resource manager context</a>. The resource manager context is set by a previous call to <see cref="SCardEstablishContext"/>.</param>
+        /// <param name="dwTimeout">The maximum amount of time to wait for an action. A value of <see cref="TimeSpan.Zero"/> causes the function to return immediately. A value of <see cref="System.Threading.Timeout.InfiniteTimeSpan"/> causes this function never to time out.</param>
+        /// <param name="rgReaderStates">
+        /// <para>An array of <see cref="SCARD_READERSTATE"/> structures that specify the readers to watch, and that receives the result.</para>
+        /// <para>To be notified of the arrival of a new smart card reader, set the <see cref="SCARD_READERSTATE.szReader"/> member of a <see cref="SCARD_READERSTATE"/> structure to <c>"\\?PnP?\Notification"</c>, and set all of the other members of that structure to zero.</para>
+        /// <para>
+        /// <note type="important">
+        /// Each member of each structure in this array must be initialized to zero and then set to specific values as necessary. If this is not done, the function will fail in situations that involve remote card readers.
+        /// </note>
+        /// </para>
+        /// </param>
+        /// <returns>
+        /// <para>If the function succeeds, the function returns <see cref="SCARD_S_SUCCESS"/>.</para>
+        /// <para>If the function fails, it returns an error code. For more information, see <a href="https://docs.microsoft.com/windows/desktop/SecAuthN/authentication-return-values">Smart Card Return Values</a>.</para>
+        /// </returns>
+        /// <remarks>
+        /// <para>The <see cref="SCardGetStatusChange(SCARDCONTEXT, TimeSpan, Span{SCARD_READERSTATE})"/> function is a <a href="https://docs.microsoft.com/windows/desktop/SecGloss/s-gly">smart card</a> tracking function. For more information about other tracking functions, see <a href="https://docs.microsoft.com/windows/desktop/SecAuthN/smart-card-tracking-functions">Smart Card Tracking Functions</a>.</para>
+        /// <para>
+        /// <list type="table">
+        /// <listheader><term>Requirements</term></listheader>
+        /// <item><term><strong>Minimum supported client:</strong></term><description>Windows XP [desktop apps only]</description></item>
+        /// <item><term><strong>Minimum supported server:</strong></term><description>Windows Server 2003 [desktop apps only]</description></item>
+        /// </list>
+        /// </para>
+        /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/win32/api/winscard/nf-winscard-scardgetstatuschangew">SCardGetStatusChangeW function</a></para>
+        /// </remarks>
+        /// <exception cref="DllNotFoundException">The native library containg the function could not be found.</exception>
+        /// <exception cref="EntryPointNotFoundException">Unable to find the entry point for the function in the native library.</exception>
+        /// <seealso cref="SCARD_READERSTATE"/>
+        /// <seealso cref="SCardCancel"/>
+        /// <seealso cref="SCardEstablishContext"/>
+        /// <seealso cref="SCardLocateCards(SCARDCONTEXT, string, Span{SCARD_READERSTATE})"/>
+        public static unsafe int SCardGetStatusChange(
+            SCARDCONTEXT hContext,
+            TimeSpan dwTimeout,
+            Span<SCARD_READERSTATE> rgReaderStates
+            )
+        {
+            fixed (SCARD_READERSTATE* ptrReaderStates = rgReaderStates)
+                return SCardGetStatusChange(
+                    hContext,
+                    (int)dwTimeout.TotalMilliseconds,
+                    ptrReaderStates,
+                    rgReaderStates.Length
+                    );
+        }
+        [DllImport(WinSCard, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        private static extern unsafe int SCardGetStatusChange(
+            [In] SCARDCONTEXT hContext,
+            [In] int dwTimeout,
             [In, Out] SCARD_READERSTATE* rgReaderStates,
             [In] int cReaders
             );

6036a253dd07cf0e996e5761fcaaf99e0fae0919
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-02-06 20:58:10 +0100

    DEVPROPSTORE Enumeration, devpropdef.h line 141

---
+++ src/THNETII.WinApi.Headers.DevPropDef/DEVPROPSTORE.cs
@@ -0,0 +1,14 @@
+namespace THNETII.WinApi.Native.DevPropDef
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\devpropdef.h, line 128
+    //
+    // DEVPROPSTORE Enumeration
+    /// <summary>
+    /// This enumeration describes where a property is stored.
+    /// </summary>
+    public enum DEVPROPSTORE
+    {
+        DEVPROP_STORE_SYSTEM,
+        DEVPROP_STORE_USER
+    }
+}

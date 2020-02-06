5044b3364e7c9dfc2543b3fa0614a968ce5f4ef5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-02-06 23:22:57 +0100

    IOCTL_KS_* constants, ks.h line 82

--- src/THNETII.WinApi.Headers.Ks/KsConstants.cs
+++ src/THNETII.WinApi.Headers.Ks/KsConstants.cs
@@ -1,7 +1,12 @@
 using System;
 
+using THNETII.WinApi.Native.WinIOCtl;
+
 namespace THNETII.WinApi.Native.Ks
 {
+    using static WinIOCtlConstants;
+    using static WinIOCtlFunctions;
+
     // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\ks.h
     /// <summary>
     /// Windows Driver Model/Connection and Streaming Architecture (WDM-CSA)
@@ -11,5 +16,18 @@ namespace THNETII.WinApi.Native.Ks
     {
         // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\ks.h, line 62
         public static readonly Guid GUID_NULL = Guid.Parse("00000000-0000-0000-0000-000000000000");
+
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\ks.h, line 72
+        //===========================================================================
+
+        public static readonly int IOCTL_KS_PROPERTY = CTL_CODE(FILE_DEVICE_KS, 0x000, METHOD_NEITHER, FILE_ANY_ACCESS);
+        public static readonly int IOCTL_KS_ENABLE_EVENT = CTL_CODE(FILE_DEVICE_KS, 0x001, METHOD_NEITHER, FILE_ANY_ACCESS);
+        public static readonly int IOCTL_KS_DISABLE_EVENT = CTL_CODE(FILE_DEVICE_KS, 0x002, METHOD_NEITHER, FILE_ANY_ACCESS);
+        public static readonly int IOCTL_KS_METHOD = CTL_CODE(FILE_DEVICE_KS, 0x003, METHOD_NEITHER, FILE_ANY_ACCESS);
+        public static readonly int IOCTL_KS_WRITE_STREAM = CTL_CODE(FILE_DEVICE_KS, 0x004, METHOD_NEITHER, FILE_WRITE_ACCESS);
+        public static readonly int IOCTL_KS_READ_STREAM = CTL_CODE(FILE_DEVICE_KS, 0x005, METHOD_NEITHER, FILE_READ_ACCESS);
+        public static readonly int IOCTL_KS_RESET_STATE = CTL_CODE(FILE_DEVICE_KS, 0x006, METHOD_NEITHER, FILE_ANY_ACCESS);
+
+        //===========================================================================
     }
 }

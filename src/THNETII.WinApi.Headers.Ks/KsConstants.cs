cfa0946c971ade0b7b43d0f6030f6dc9f2c1c7b4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-02-06 23:18:35 +0100

    GUID_NULL constant, ks.h line 64

---
+++ src/THNETII.WinApi.Headers.Ks/KsConstants.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace THNETII.WinApi.Native.Ks
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\ks.h
+    /// <summary>
+    /// Windows Driver Model/Connection and Streaming Architecture (WDM-CSA)
+    /// core definitions.
+    /// </summary>
+    public static class KsConstants
+    {
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared\ks.h, line 62
+        public static readonly Guid GUID_NULL = Guid.Parse("00000000-0000-0000-0000-000000000000");
+    }
+}

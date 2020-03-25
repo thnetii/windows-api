975d5b59cc7c503b79f904ed5744144e47e5123f
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-03-25 14:02:56 +0100

    Empty ACCESS_RIGHTS and INHERIT_FLAGS types, accctrl.h line 375

---
+++ src/THNETII.WinApi.Headers.AccCtrl/ACCESS_RIGHTS.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace THNETII.WinApi.Native.AccCtrl
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\AccCtrl.h, line 367
+    //
+    // Access rights
+    //
+
+    [Flags]
+    public enum ACCESS_RIGHTS : int
+    {
+    }
+}
---
+++ src/THNETII.WinApi.Headers.AccCtrl/INHERIT_FLAGS.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace THNETII.WinApi.Native.AccCtrl
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\AccCtrl.h, line 372
+    //
+    // Inheritance flags
+    //
+
+    [Flags]
+    public enum INHERIT_FLAGS : int
+    {
+    }
+}

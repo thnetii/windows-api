92e0b5f74bc8300dd2c52633f01b97306f1b7ecd
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-03-27 17:34:51 +0100

    Fixes for FileApi

--- src-native/THNETII.WinApi.Headers.FileApi/GlobalSuppressions.cs
+++ src-native/THNETII.WinApi.Headers.FileApi/GlobalSuppressions.cs
@@ -1,11 +1,12 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Naming", "CA1707: Identifiers should not contain underscores")]
-[assembly: SuppressMessage("Interoperability", "CA1401: P/Invokes should not be visible")]
+[assembly: SuppressMessage("Documentation", "CS0419: Ambiguous reference in cref attribute")]
 [assembly: SuppressMessage("Globalization", "CA2101: Specify marshaling for P/Invoke string arguments", Justification = "Erroneously reported as not specified")]
+[assembly: SuppressMessage("Interoperability", "CA1401: P/Invokes should not be visible")]
+[assembly: SuppressMessage("Naming", "CA1707: Identifiers should not contain underscores")]
 [assembly: SuppressMessage("Usage", "PC003: Native API not available in UWP")]

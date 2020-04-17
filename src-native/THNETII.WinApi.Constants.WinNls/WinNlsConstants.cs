13ff7ba82c01cf6995c140965ad1ebb37b466860
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-04-17 16:29:04 +0200

    Add WinNLS projects

---
+++ src-native/THNETII.WinApi.Constants.WinNls/WinNlsConstants.cs
@@ -0,0 +1,15 @@
+namespace THNETII.WinApi.Native.WinNls
+{
+    // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\WinNls.h
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    //  Constants
+    //
+    //  Define all constants for the NLS component here.
+    //
+    ////////////////////////////////////////////////////////////////////////////
+
+    public static class WinNlsConstants
+    {
+    }
+}

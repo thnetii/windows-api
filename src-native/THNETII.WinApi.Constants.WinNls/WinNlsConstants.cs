4b013c8aae294ae603b78a2c649af1acbd7d2e97
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-04-17 16:32:43 +0200

    Add WinNLS constants, winnls.h line 102

--- src-native/THNETII.WinApi.Constants.WinNls/WinNlsConstants.cs
+++ src-native/THNETII.WinApi.Constants.WinNls/WinNlsConstants.cs
@@ -11,5 +11,11 @@ namespace THNETII.WinApi.Native.WinNls
 
     public static class WinNlsConstants
     {
+        // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\WinNls.h, line 98
+        //
+        //  String Length Maximums.
+        //
+        public const int MAX_LEADBYTES = 12;    // 5 ranges, 2 bytes ea., 0 term.
+        public const int MAX_DEFAULTCHAR = 2;   // single or double byte
     }
 }

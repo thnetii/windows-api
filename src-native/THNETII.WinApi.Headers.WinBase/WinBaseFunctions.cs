7f97711470f0ab4a8923b8c4ef656548a1962d75
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-04-25 16:19:52 +0200

    Add documentation for FormatMessage functions

--- src-native/THNETII.WinApi.Headers.WinBase/WinBaseFunctions.cs
+++ src-native/THNETII.WinApi.Headers.WinBase/WinBaseFunctions.cs
@@ -1186,6 +1186,149 @@ namespace THNETII.WinApi.Native.WinBase
         // C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um\WinBase.h, line 2413
         #region FormatMessage function
         // string lpSource
+#pragma warning disable CS1572 // XML comment has a param tag, but there is no parameter by that name
+#pragma warning disable CS1734 // XML comment has a paramref tag, but there is no parameter by that name
+        /// <summary>
+        /// Formats a message string. The function requires a message definition as input. The message definition can come from a buffer passed into the function. It can come from a message table resource in an already-loaded module. Or the caller can ask the function to search the system's message table resource(s) for the message definition. The function finds the message definition in a message table resource based on a message identifier and a language identifier. The function copies the formatted message text to an output buffer, processing any embedded insert sequences if requested.
+        /// </summary>
+        /// <param name="dwFlags">
+        /// The formatting options, and how to interpret the <paramref name="lpSource"/> parameter.
+        /// <list type="table">
+        /// <listheader><term>Value</term><description>Meaning</description></listheader>
+        /// <item><term><see cref="FORMAT_MESSAGE_ALLOCATE_BUFFER"/></term>
+        /// <description>
+        /// <para>Automatically set or cleared by the function as appropriate by the specific overload.</para>
+        /// <para>The function allocates a buffer large enough to hold the formatted message, and places the allocated buffer at the address specified by <paramref name="lpBuffer"/>. The <paramref name="nSize"/> parameter specifies the minimum number of characters to allocate for an output message buffer. The caller should use the <see cref="LocalFree"/> function to free the buffer when it is no longer needed.</para>
+        /// <para>If the length of the formatted message exceeds 128K bytes, then <see cref="FormatMessage"/> will fail and a subsequent call to <see cref="Marshal.GetLastWin32Error"/> will return <see cref="ERROR_MORE_DATA"/>.</para>
+        /// <para>In previous versions of Windows, this value was not available for use when compiling Windows Store apps. As of Windows 10 this value can be used.</para>
+        /// <para><strong>Windows Server 2003 and Windows XP</strong>: If the length of the formatted message exceeds 128K bytes, then <see cref="FormatMessage"/> will not automatically fail with an error of <see cref="ERROR_MORE_DATA"/>.</para>
+        /// </description></item>
+        /// <item><term><see cref="FORMAT_MESSAGE_ARGUMENT_ARRAY"/></term><description>
+        /// <para>Automatically applied by the function, as variadic argument invocation is not supported.</para>
+        /// </description></item>
+        /// <item><term><see cref="FORMAT_MESSAGE_FROM_HMODULE"/></term><description>
+        /// <para>Automatically cleared by overloads that accept a <see cref="string"/>, <see cref="char"/> pointer or <see cref="byte"/> pointer value for the <paramref name="lpSource"/> parameter.</para>
+        /// <para>The <paramref name="lpSource"/> parameter is a module handle containing the message-table resource(s) to search. If this <paramref name="lpSource"/> handle is <see cref="IntPtr.Zero"/>, the current process's application image file will be searched. This flag cannot be used with <see cref="FORMAT_MESSAGE_FROM_STRING"/>.</para>
+        /// <para>If the module has no message table resource, the function fails with <see cref="ERROR_RESOURCE_TYPE_NOT_FOUND"/>.</para>
+        /// </description></item>
+        /// <item><term><see cref="FORMAT_MESSAGE_FROM_STRING"/></term><description>
+        /// <para>Automatically set by overloads that accept a <see cref="string"/>, <see cref="char"/> pointer or <see cref="byte"/> pointer value for the <paramref name="lpSource"/> parameter.</para>
+        /// <para>The <paramref name="lpSource"/> parameter is a <see cref="string"/> value or a pointer to a null-terminated string that contains a message definition. The message definition may contain insert sequences, just as the message text in a message table resource may. This flag cannot be used with <see cref="FORMAT_MESSAGE_FROM_HMODULE"/> or <see cref="FORMAT_MESSAGE_FROM_SYSTEM"/>.</para>
+        /// </description></item>
+        /// <item><term><see cref="FORMAT_MESSAGE_FROM_SYSTEM"/></term><description>
+        /// <para>Automatically cleared by overloads that accept a <see cref="string"/>, <see cref="char"/> pointer or <see cref="byte"/> pointer value for the <paramref name="lpSource"/> parameter.</para>
+        /// <para>The function should search the system message-table resource(s) for the requested message. If this flag is specified with <see cref="FORMAT_MESSAGE_FROM_HMODULE"/>, the function searches the system message table if the message is not found in the module specified by <paramref name="lpSource"/>. This flag cannot be used with <see cref="FORMAT_MESSAGE_FROM_STRING"/>.</para>
+        /// <para>If this flag is specified, an application can pass the result of the <see cref="Marshal.GetLastWin32Error"/> function to retrieve the message text for a system-defined error.</para>
+        /// </description></item>
+        /// <item><term><see cref="FORMAT_MESSAGE_IGNORE_INSERTS"/></term><description>
+        /// <para>Insert sequences in the message definition are to be ignored and passed through to the output buffer unchanged. This flag is useful for fetching a message for later formatting. If this flag is set, the <paramref name="Arguments"/> parameter is ignored.</para>
+        /// </description></item>
+        /// </list>
+        /// </param>
+        /// <param name="dwWidth">
+        /// Can be used to specify the maximum width of a formatted output line.
+        /// Valid values range from and including <c>0</c> (zero) up to but excluding <c>256</c>
+        /// (the range of the <see cref="byte"/> type). The Value is truncated to fit into this range.
+        /// <para>If the value is a non-zero value other than <see cref="WinBaseConstants.FORMAT_MESSAGE_MAX_WIDTH_MASK"/>, it specifies the maximum number of characters in an output line. The function ignores regular line breaks in the message definition text. The function never splits a string delimited by white space across a line break. The function stores hard-coded line breaks in the message definition text into the output buffer. Hard-coded line breaks are coded with the <c>%n</c> escape sequence.</para>
+        /// <para>
+        /// The following values have special meaning.
+        /// <list type="table">
+        /// <listheader><term>Value</term><description>Meaning</description></listheader>
+        /// <item><term><c>0</c> (zero)</term><description>There are no output line width restrictions. The function stores line breaks that are in the message definition text into the output buffer.</description></item>
+        /// <item><term><see cref="WinBaseConstants.FORMAT_MESSAGE_MAX_WIDTH_MASK"/><br/>(<c>0x000000FF</c>)</term><description>The function ignores regular line breaks in the message definition text. The function stores hard-coded line breaks in the message definition text into the output buffer. The function generates no new line breaks.</description></item>
+        /// </list>
+        /// </para>
+        /// </param>
+        /// <param name="lpSource">
+        /// The location of the message definition. The type of this parameter depends upon the settings in the <paramref name="dwFlags"/> parameter.
+        /// <list type="table">
+        /// <listheader><term><paramref name="dwFlags"/> Setting</term><description>Meaning</description></listheader>
+        /// <item><term><see cref="FORMAT_MESSAGE_FROM_HMODULE"/></term><description>A handle to the module that contains the message table to search.</description></item>
+        /// <item><term><see cref="FORMAT_MESSAGE_FROM_STRING"/></term><description>Pointer or reference to a string that consists of unformatted message text. It will be scanned for inserts and formatted accordingly.</description></item>
+        /// </list>
+        /// If neither of these flags is set in <paramref name="dwFlags"/>, then <paramref name="lpSource"/> is ignored.
+        /// </param>
+        /// <param name="dwMessageId">The message identifier for the requested message. This parameter is ignored if <paramref name="dwFlags"/> includes <see cref="FORMAT_MESSAGE_FROM_STRING"/>.</param>
+        /// <param name="dwLanguageId">
+        /// <para>The <a href="https://docs.microsoft.com/windows/desktop/Intl/language-identifiers">language identifier</a> for the requested message. This parameter is ignored if <paramref name="dwFlags"/> includes <see cref="FORMAT_MESSAGE_FROM_STRING"/>.</para>
+        /// <para>
+        /// If you pass a specific <c>LANGID</c> in this parameter, <see cref="FormatMessage"/> will return a message for that <c>LANGID</c> only. If the function cannot find a message for that <c>LANGID</c>, it sets Last-Error to <see cref="ERROR_RESOURCE_LANG_NOT_FOUND"/>. If you pass in zero, <see cref="FormatMessage"/> looks for a message for <c>LANGID</c>s in the following order:
+        /// <list type="number">
+        /// <item>Language neutral</item>
+        /// <item>Thread <c>LANGID</c>, based on the thread's locale value</item>
+        /// <item>User default <c>LANGID</c>, based on the user's default locale value</item>
+        /// <item>System default <c>LANGID</c>, based on the system default locale value</item>
+        /// <item>US English</item>
+        /// </list>
+        /// </para>
+        /// <para>If <see cref="FormatMessage"/> does not locate a message for any of the preceding <c>LANGID</c>s, it returns any language message string that is present. If that fails, it returns <see cref="ERROR_RESOURCE_LANG_NOT_FOUND"/>.</para>
+        /// </param>
+        /// <param name="lpBuffer">
+        /// <para>A buffer that receives the null-terminated string that specifies the formatted message. If <paramref name="dwFlags"/> includes <see cref="FORMAT_MESSAGE_ALLOCATE_BUFFER"/>, the function allocates a buffer using the <see cref="LocalAlloc"/> function, and places the pointer to the buffer at the address specified in <paramref name="lpBuffer"/>.</para>
+        /// <para>This buffer cannot be larger than 64K bytes.</para>
+        /// </param>
+        /// <param name="nSize">
+        /// <para>If the <see cref="FORMAT_MESSAGE_ALLOCATE_BUFFER"/> flag is not set, this parameter specifies the size of the output buffer, in characters. If <see cref="FORMAT_MESSAGE_ALLOCATE_BUFFER"/> is set, this parameter specifies the minimum number of characters to allocate for an output buffer.</para>
+        /// <para>The output buffer cannot be larger than 64K bytes.</para>
+        /// </param>
+        /// <param name="Arguments">
+        /// <para>An array of values that are used as insert values in the formatted message. A <c>%1</c> in the format string indicates the first value in the Arguments array; a <c>%2</c> indicates the second argument; and so on.</para>
+        /// <para>The interpretation of each value depends on the formatting information associated with the insert in the message definition. The default is to treat each value as a pointer to a null-terminated string.</para>
+        /// <para>The function automatically applies the <see cref="FORMAT_MESSAGE_ARGUMENT_ARRAY"/> flag.</para>
+        /// </param>
+        /// <returns>
+        /// <para>If the function succeeds, the return value is the number of characters or bytes stored in the output buffer, excluding the terminating null character.</para>
+        /// <para>If the function fails, the return value is zero. To get extended error information, call <see cref="Marshal.GetLastWin32Error"/>.</para>
+        /// </returns>
+        /// <remarks>
+        /// <para>
+        /// Within the message text, several escape sequences are supported for dynamically formatting the message. These escape sequences and their meanings are shown in the following tables. All escape sequences start with the percent character (<c>%</c>).
+        /// <list type="table">
+        /// <listheader><term>Escape sequence</term><description>Meaning</description></listheader>
+        /// <item><term><c>%0</c></term><description>Terminates a message text line without a trailing new line character. This escape sequence can be used to build up long lines or to terminate the message itself without a trailing new line character. It is useful for prompt messages.</description></item>
+        /// <item><term><c>%<var>n</var>!<var>format string</var>!</c></term><description>
+        /// <para>Identifies an insert. The value of <var>n</var> can be in the range from <c>1</c> through <c>99</c>. The <var>format string</var> (which must be surrounded by exclamation marks) is optional and defaults to <c>!s!</c> if not specified. For more information, see <a href="https://msdn.microsoft.com/library/56e442dc.aspx">Format Specification Fields</a>.</para>
+        /// <para>The format string can include a width and precision specifier for strings and a width specifier for integers. Use an asterisk (<c>*</c>) to specify the width and precision. For example, <c>%1!.*s!</c> or <c>%1!*u!</c>.</para>
+        /// <para>If you do not use the width and precision specifiers, the insert numbers correspond directly to the input arguments. For example, if the source string is <c>"%1 %2 %1"</c> and the input arguments are <c>"Bill"</c> and <c>"Bob"</c>, the formatted output string is <c>"Bill Bob Bill"</c>.</para>
+        /// <para>However, if you use a width and precision specifier, the insert numbers do not correspond directly to the input arguments. For example, the insert numbers for the previous example could change to <c>"%1!*.*s! %4 %5!*s!"</c>.</para>
+        /// <para>For an arguments array, the next insert number is <c>n+2</c> if the previous format string contained one asterisk and is <c>n+3</c> if two asterisks were specified.</para>
+        /// <para>If you want to repeat <c>"Bill"</c>, as in the previous example, the arguments must include <c>"Bill"</c> twice. For example, if the source string is <c>"%1!*.*s! %4 %5!*s!"</c>, the arguments could be, <c>4</c>, <c>2</c>, <c>Bill</c>, <c>Bob</c>, <c>6</c>, <c>Bill</c> (if using the <see cref="FORMAT_MESSAGE_ARGUMENT_ARRAY"/> flag). The formatted string would then be <c>"  Bi Bob   Bill"</c>.</para>
+        /// <para>Repeating insert numbers when the source string contains width and precision specifiers may not yield the intended results. If you replaced <c>%5</c> with <c>%1</c>, the function would try to print a string at address <c>6</c> (likely resulting in an access violation).</para>
+        /// <para>Floating-point format specifiers — <c>e</c>, <c>E</c>, <c>f</c>, and <c>g</c> — are not supported. The workaround is to use the <see cref="StringCchPrintf"/> function to format the floating-point number into a temporary buffer, then use that buffer as the insert string.</para>
+        /// <para>Inserts that use the <c>I64</c> prefix are treated as two 32-bit arguments. They must be used before subsequent arguments are used. Note that it may be easier for you to use <see cref="StringCchPrintf"/> instead of this prefix.</para>
+        /// </description></item>
+        /// </list>
+        /// Any other nondigit character following a percent character is formatted in the output message without the percent character. Following are some examples.
+        /// <list type="table">
+        /// <listheader><term>Format string</term><description>Resulting output</description></listheader>
+        /// <item><term><c>%%</c></term><description>A single percent sign.</description></item>
+        /// <item><term><c>%<em>space</em></c></term><description>A single space. This format string can be used to ensure the appropriate number of trailing spaces in a message text line.</description></item>
+        /// <item><term><c>%.</c></term><description>A single period. This format string can be used to include a single period at the beginning of a line without terminating the message text definition.</description></item>
+        /// <item><term><c>%!</c></term><description>A single exclamation point. This format string can be used to include an exclamation point immediately after an insert without its being mistaken for the beginning of a format string.</description></item>
+        /// <item><term><c>%n</c></term><description>A hard line break when the format string occurs at the end of a line. This format string is useful when <see cref="FormatMessage"/> is supplying regular line breaks so the message fits in a certain width.</description></item>
+        /// <item><term><c>%r</c></term><description>A hard carriage return without a trailing newline character.</description></item>
+        /// <item><term><c>%t</c></term><description>A single tab.</description></item>
+        /// </list>
+        /// </para>
+        /// <para><note type="security">
+        /// If this function is called without <see cref="FORMAT_MESSAGE_IGNORE_INSERTS"/>, the <paramref name="Arguments"/> parameter must contain enough parameters to satisfy all insertion sequences in the message string, and they must be of the correct type. Therefore, do not use untrusted or unknown message strings with inserts enabled because they can contain more insertion sequences than <paramref name="Arguments"/> provides, or those that may be of the wrong type. In particular, it is unsafe to take an arbitrary system error code returned from an API and use <see cref="FORMAT_MESSAGE_FROM_SYSTEM"/> without <see cref="FORMAT_MESSAGE_IGNORE_INSERTS"/>.
+        /// </note></para>
+        /// <para>
+        /// <list type="table">
+        /// <listheader><term>Requirements</term></listheader>
+        /// <item><term><strong>Minimum supported client:</strong></term><description>Windows XP [desktop apps| UWP apps]</description></item>
+        /// <item><term><strong>Minimum supported server:</strong></term><description>Windows Server 2003 [desktop apps| UWP apps]</description></item>
+        /// </list>
+        /// </para>
+        /// <para>Microsoft Docs page: <a href="https://docs.microsoft.com/en-us/windows/desktop/api/winbase/nf-winbase-formatmessagew">FormatMessageW function</a></para>
+        /// </remarks>
+        /// <exception cref="DllNotFoundException">The native library containg the function could not be found.</exception>
+        /// <exception cref="EntryPointNotFoundException">Unable to find the entry point for the function in the native library.</exception>
+        /// <seealso href="https://docs.microsoft.com/windows/desktop/Debug/error-handling-functions">Error Handling Functions</seealso>
+        /// <seealso href="https://docs.microsoft.com/windows/desktop/WES/message-compiler--mc-exe-">Message Compiler</seealso>
+        /// <seealso>Message Tables</seealso>
+#pragma warning restore CS1572 // XML comment has a param tag, but there is no parameter by that name
+#pragma warning restore CS1734 // XML comment has a paramref tag, but there is no parameter by that name
         public static unsafe int FormatMessage(
             FORMAT_MESSAGE_FLAGS dwFlags,
             int dwWidth,
